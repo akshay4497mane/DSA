893b79b365db72d14454204b0350bab9f0a78060
Akshay Mane <akshay4497mane@gmail.com>
2023-09-08 00:43:15 +0530

    Time: 381 ms (5.00%) | Memory: 58.4 MB (63.82%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,12 +1,29 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int N = nums.length, summ = 0, maxSum = Integer.MIN_VALUE;
-        
+        int N = nums.length, currSum = 0, maxSum = Integer.MIN_VALUE;
+        /*//Kadane's Algorithm Return Maximum possible Sum for a Continuous SubArray
         for(int i = 0; i<N ;i++){
-            summ += nums[i];
-            summ = Math.max( summ, nums[i] );
-            maxSum = Math.max( summ, maxSum);
+            currSum = Math.max( currSum + nums[i] , nums[i] );
+            maxSum = Math.max( currSum, maxSum);
         }
+        */
+        int maxStart =0, maxEnd = 0, currStart = 0, currEnd = 0;
+        for(int i = 0; i < N ;i++){
+            if( currSum + nums[i] > nums[i] ){//Add nums[i]
+                currSum = currSum + nums[i]; currEnd = i;
+                if(currSum > maxSum){
+                    maxSum = currSum; maxStart = currStart; maxEnd = currEnd;
+                }
+            }else{//Dont add nums[i], start new subArray from nums[i]
+                currSum = nums[i]; currStart = currEnd = i;
+                if(currSum > maxSum){
+                    maxSum = currSum; maxStart = currStart; maxEnd = currEnd;
+                }
+            }
+        }
+        System.out.print("Max Sum SubARRAY: ");
+        for(int i= maxStart; i<= maxEnd; i++)
+            System.out.print(nums[i]);
         return maxSum;
     }
 }
