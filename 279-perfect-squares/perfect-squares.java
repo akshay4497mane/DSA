3f36190041379e55ca510cb81f2c4adc9faedf7c
Akshay Mane <akshay4497mane@gmail.com>
2023-11-09 02:06:24 +0530

    Time: 19 ms (94.63%) | Memory: 42.7 MB (42.95%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -8,7 +8,10 @@ f(N) = 1 + min(f(N-1^2), f(N-2^2), f(N-3^2).... )
 f(12) = 1+ min(f(11), f(8), f(3) )
 */
 
+    /*
     //DP Top Down Recursive | Time: O( N Sqrt(N) ) | Space: O( N )
+        //Runtime 69ms | Beats 26.55%of users with Java
+        //Memory 41.31MB | Beats 90.64%of users with Java
     public int numSquares(int n) {
         if(n==0) return 0; // or if(n<=2) return n;
         int[] dp = new int[n+1];
@@ -25,6 +28,23 @@ f(12) = 1+ min(f(11), f(8), f(3) )
             tempAns = Math.min( tempAns, helper(n-x*x, dp) );
         }
         dp[n] = 1 + tempAns;
+        return dp[n];
+    }
+    */
+    //DP Bottom UP iterative | Time: O(N Sqrt(N)) | Space: O(N)
+    public int numSquares(int n) {
+        if(n==0) return 0; // or if(n<=2) return n;
+        int[] dp = new int[n+1];
+
+        dp[0] = 0;
+        int tempAns =0;
+        for( int i=1; i<=n; i++ ){
+            tempAns =Integer.MAX_VALUE;
+            for(int x=1; x*x <= i ; x++){
+                tempAns = Math.min(tempAns , dp[i-x*x] );
+            }
+            dp[i] = 1+tempAns;
+        }
         return dp[n];
     }
 }
