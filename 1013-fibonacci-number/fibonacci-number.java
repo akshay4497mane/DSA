aac10356a4082d3919e5779afff60bf7d5c60cd6
Akshay Mane <akshay4497mane@gmail.com>
2023-11-09 00:39:12 +0530

    Time: 0 ms (100.00%) | Memory: 39.3 MB (31.17%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,13 +1,18 @@
 class Solution {
     /*
-    // Recursive Time: O(2^N). Space: O()
+    // Recursive Fibonacci Time: O(2^N). Space: O(N)
+        //Runtime 6ms | Beats 48.21%of users with Java
+        //Memory 38.86MB | Beats 79.20%of users with Java
     public int fib(int n) {
         if(n<2) return n;
         return fib(n-1) + fib(n-2);
     }
-    //Runtime 6ms | Beats 48.21%of users with Java
-    //Memory 38.86MB | Beats 79.20%of users with Java
     */
+
+    /*
+    //DP Top Down| Recursion + Memoization =>  Time: O(N), Space: O(N + N)
+    //Runtime 0ms | Beats 100.00%of users with Java
+    //Memory | 39.02MB | Beats 53.65%of users with Java
     public int fib(int n) {
         int[] DP = new int[n+1];
         for(int i=0; i<DP.length; i++)
@@ -22,4 +27,19 @@ class Solution {
             dp[n] = fibHelper(n-1, dp) + fibHelper(n-2, dp);
         return dp[n];
     }
+    */
+
+    //DP Bottom Up | Tabulation O(N) space
+    public int fib(int n) {
+        if(n<2) return n;
+        
+        int[] DP = new int[n+1];
+        DP[0] = 0;
+        DP[1] = 1;
+        for(int i=2; i<=n; i++)
+            DP[i] = DP[i-1] + DP[i-2];
+        return DP[n];
+    }
+
+
 }
