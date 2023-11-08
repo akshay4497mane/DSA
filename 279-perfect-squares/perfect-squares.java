b739f040db01de70bb817ef168e7b4eef4b27de8
Akshay Mane <akshay4497mane@gmail.com>
2023-11-09 01:49:42 +0530

    Time: 69 ms (26.55%) | Memory: 41.3 MB (90.64%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.java
@@ -0,0 +1,30 @@
+class Solution {
+/*
+N=6, 111111, 211 => ans =3
+N=10, 1=> 10, 2111111 => 6,2211 => 4, 31 => ans =2
+N=9 : 3 => ans 1
+N=12, 3111 => 4, 222 => ans =3 | greedy fails 
+f(N) = 1 + min(f(N-1^2), f(N-2^2), f(N-3^2).... )
+f(12) = 1+ min(f(11), f(8), f(3) )
+*/
+
+    //DP Top Down Recursive | Time: O( N Sqrt(N) ) | Space: O( N )
+    public int numSquares(int n) {
+        if(n==0) return 0; // or if(n<=2) return n;
+        int[] dp = new int[n+1];
+        for(int i=0;i<=n;i++)
+            dp[i] = -1;
+        return helper(n, dp);
+    }
+    public int helper(int n, int[] dp){
+        if(n==0) return 0; // or if(n<=2) return n;
+        if(dp[n] != -1)
+            return dp[n];
+        int tempAns = Integer.MAX_VALUE;
+        for(int x=1; x<=Math.sqrt(n); x++){
+            tempAns = Math.min( tempAns, helper(n-x*x, dp) );
+        }
+        dp[n] = 1 + tempAns;
+        return dp[n];
+    }
+}
