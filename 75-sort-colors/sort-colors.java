d0675ec2995dbf7f539e1a00bd34cc88c86a74e6
Akshay Mane <akshay4497mane@gmail.com>
2023-12-25 18:10:10 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (7.21%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,6 +1,31 @@
-class Solution { //Sort an array of colors using COUNTING SORT
+class Solution {
+    // Function to sort an array of colors in-place using DUTCH NATIONAL FLAG ALGORITHM
+    public void sortColors(int[] A) {// Time Complexity: O(N), Space Complexity: O(1)
+        int N = A.length, p1 = 0, p2 = N - 1, i = 0;
+        if (N <= 1)
+            return;
+        while (i <= p2) {
+            if (A[i] == 0) {
+                swap(A, i, p1);
+                i++;
+                p1++;
+            } else if (A[i] == 2) {
+                swap(A, i, p2);
+                p2--; //not incrementing i
+            } else { // A[i] == 1
+                i++;
+            }
+        }
+    }
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    //Sort an array of colors using COUNTING SORT
     // Time Complexity: O(N), Space Complexity: O(numColors)
-    public void sortColors(int[] A) {
+    public void sortColors2PassCountingSort(int[] A) {
         int numColors = 3; // Number of colors in the input array
         int[] count = new int[numColors];
         int N = A.length; // Length of the input array
