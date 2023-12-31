bfe97da959e40687c43d7881033cfa190ecdcefe
Akshay Mane <akshay4497mane@gmail.com>
2023-12-31 16:07:07 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (12.22%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.java
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -9,19 +9,28 @@
  * }
  */
 class Solution {
-    public ListNode middleNode(ListNode head) {        
+    //Approach 1: Calculate N, traverse N/2, Time: O(N), 2 passes, Space: O(1)
+    public ListNode middleNode2Pass(ListNode head) {
         if(head == null || head.next == null) return head;
-        //Find count N
-        int N=0;
-        ListNode curr = head;
-        while( curr != null ){
+        int N=0; ListNode curr = head;
+        while( curr != null ){      //Find count N
             N++; curr = curr.next;
         }
         N = N/2; curr = head;
-        while( N > 0 ){
-            curr = curr.next;
-            N--; 
+        while( N > 0 ){  //Traverse N/2 mid
+            N--; curr = curr.next;
         }
         return curr;
+    }
+
+    //Approach 2: TWO Pointers, Time: O(N), 1 pass, Space: O(1)
+    public ListNode middleNode(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode slow = head, fast = head;
+        while(fast != null && fast.next != null ){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
     }
 }
