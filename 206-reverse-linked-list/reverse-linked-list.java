7c42bc504a5edd74fd773980b149aa119ae91af9
Akshay Mane <akshay4497mane@gmail.com>
2023-12-31 15:37:54 +0530

    Time: 2 ms (100.00%) | Memory: 42.4 MB (11.40%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -9,10 +9,36 @@
  * }
  */
 class Solution {
-    //iterative Reverse LL
+    //Recursive
+    public ListNode reverseListRecursive(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode ans = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return ans;        
+    }
+    //Iterative Using Stack Space : O(N)
     public ListNode reverseList(ListNode head) {
-        if(head == null) return null;
-        if(head.next == null) return head;
+        if(head == null || head.next == null) return head;
+        Stack<ListNode> st = new Stack<>();
+        ListNode curr = head;
+        while(curr != null){
+            st.push(curr);
+            curr = curr.next;
+        }
+        ListNode dummy = new ListNode(0);
+        curr = dummy;
+        while(st.size() > 0){   //(!st.isEmpty())
+            curr.next = st.pop();
+            curr = curr.next;
+        }
+        curr.next = null;
+        return dummy.next;
+    }
+
+    //iterative Space : O(1) memory | Reverse LL
+    public ListNode reverseListIterative(ListNode head) {
+        if(head == null || head.next == null) return head;
         ListNode prev = null, curr = head, next = head.next;
         while(curr != null){
             curr.next = prev;
@@ -23,15 +49,4 @@ class Solution {
         }
         return prev;        
     }
-    //Recursive
-    public ListNode reverseListRecursive(ListNode head) {
-        if(head == null) return null;
-        if(head.next == null) return head;
-        ListNode ans = reverseList(head.next);
-        head.next.next = head;
-        head.next = null;
-        return ans;        
-    }
-
-
 }
