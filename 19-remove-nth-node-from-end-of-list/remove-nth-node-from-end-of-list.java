8e22eb676b2f071f2b5026b4a523a61cfd9b928b
Akshay Mane <akshay4497mane@gmail.com>
2023-12-31 17:46:03 +0530

    Time: 1 ms (8.23%) | Memory: 42.1 MB (5.03%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -11,10 +11,21 @@
 class Solution {
     //Approach 1 : Using Recursion, 
     //Approach 2 : Using Stack, 
-    //Approach 3 : Count all, traverse count-n
-    
-    //Approach 4 : two pointer, move fast pointer n places ahead, traverse both equally
+    //Approach 3 : Count all, traverse count-n | Time: O(N), 2 passes
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode curr = head, dummy = new ListNode(0, head);
+        int N = 0;
+        while(curr != null){ N++; curr = curr.next; }        
+        curr = dummy;
+        for(int i=0; i< N-n ; i++){ //N=5, n=1-5,
+            curr = curr.next;
+        }
+        if(curr == dummy) return head.next;
+        curr.next = curr.next.next;
+        return head;
+    }
+    //Approach 4 : two pointer, move fast pointer n places ahead, traverse both equally | Time O(N) 1 pass
+    public ListNode removeNthFromEndTwoPointer(ListNode head, int n) {
         ListNode slow = head, fast = head, prev = new ListNode(0, head);
         for(; n>0 ; n--){
             fast = fast.next;
@@ -28,5 +39,4 @@ class Solution {
         prev.next = prev.next.next;
         return head;
     }
-
 }
