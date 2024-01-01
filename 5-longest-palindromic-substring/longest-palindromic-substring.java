6a17dbb4e7bd2d6e84dc46241ec5018234b51e7c
Akshay Mane <akshay4497mane@gmail.com>
2024-01-01 09:28:56 +0530

    Time: 37 ms (49.94%) | Memory: 44.9 MB (25.90%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,6 +1,9 @@
 class Solution {
-    //Approach 1 : Brute Force, Generate all substrings, check all palindrome or not | Time : O(N^3) = N^2 * N
-    public String longestPalindrome(String s) {
+/*
+    Approach 1 : Brute Force, Generate all substrings, check all palindrome or not | 
+    Time : O(N^3) = N^2 * N
+*/
+    public String longestPalindromeBruteForce(String s) {
         int n = s.length(), maxLen = 1;
         String maxStr = s.substring(0,1);
         for(int i=0; i<n ; i++){
@@ -21,9 +24,30 @@ class Solution {
         }
         return true;
     }
-    //public String longestPalindrome(String s) {
-        
-    //}
-
-
+    /*
+    APPROACH 2 :
+        for every i, 
+        see if s[i] is centre of odd palindrome or
+            if s[i]&s[i+1] is centre of even palindrome
+        Time: O(N^2)
+    */
+    
+    public String longestPalindrome(String s) {
+        String maxStr = s.substring(0,1);
+        for(int i=0; i<s.length()-1; i++){
+            String odd = expandFromCentre(s,i,i);
+            String even = expandFromCentre(s,i,i+1);
+            if(odd.length() > maxStr.length())
+                maxStr = odd;
+            if(even.length() > maxStr.length())
+                maxStr = even;            
+        }
+        return maxStr;        
+    }
+    String expandFromCentre(String s, int i, int j){
+        while( i >= 0 && j < s.length() && s.charAt(i)==s.charAt(j) ){
+            j++; i--;
+        }
+        return s.substring(i+1, j);
+    }
 }
