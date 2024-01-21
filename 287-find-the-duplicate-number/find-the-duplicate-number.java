eb02e6f828aba282a71237ec38a39945d5883e59
Akshay Mane <akshay4497mane@gmail.com>
2024-01-22 02:46:47 +0530

    Time: 1 ms (100.00%) | Memory: 58.2 MB (43.52%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,10 +1,11 @@
 class Solution {
 /*
-Approach 1 : Sort array, compare ith & i+1th elements  | Time: O(N logN)
+Approach 1 : Sort array, compare ith & i-1 th elements  | Time: O(N logN) | Space: O(1)
 Approach 2 : Count frequency of each | O(N) time but using O(N) space, boolean Array, HashMap, HashSet |  
 Approach 3 : Brute Force, Use two loops, Search each element in array, O(N^2) time
 */
-    public int findDuplicate(int[] A) {
+    //Approach 1 : Sort array, compare ith & i-1 th elements  | Time: O(N logN) | Space: O(1)
+    public int findDuplicate_Sorting(int[] A) {
         Arrays.sort(A);
         int N = A.length;
         for(int i=1; i<N ;i++)
@@ -12,6 +13,16 @@ Approach 3 : Brute Force, Use two loops, Search each element in array, O(N^2) ti
                 return A[i];
         return N;
     }
-    
+    //
+    public int findDuplicate(int[] A) {
+        int N = A.length;
+        boolean[] visited = new boolean[N+1];
+        for(int i=0;i<N;i++){
+            if(visited[A[i]] == true)
+                return A[i];
+            visited[A[i]] = true;
+        }
+        return N;
+    }
 
 }
