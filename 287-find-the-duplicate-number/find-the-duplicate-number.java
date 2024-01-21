0e936476f3a4d242d418d5f38149394a1f63972d
Akshay Mane <akshay4497mane@gmail.com>
2024-01-22 03:01:11 +0530

    Time: 35 ms (14.67%) | Memory: 56.8 MB (72.60%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -3,6 +3,7 @@ class Solution {
 Approach 1 : Sort array, compare ith & i-1 th elements  | Time: O(N logN) | Space: O(1)
 Approach 2 : Count frequency of each | O(N) time but using O(N) space, boolean Array, HashMap, HashSet |  
 Approach 3 : Brute Force, Use two loops, Search each element in array, O(N^2) time
+Approach 4 : O(1) space | Instead of boolean array, Use array itself to store Visited by Marking values as -ve
 */
     //Approach 1 : Sort array, compare ith & i-1 th elements  | Time: O(N logN) | Space: O(1)
     public int findDuplicate_Sorting(int[] A) {
@@ -13,8 +14,8 @@ Approach 3 : Brute Force, Use two loops, Search each element in array, O(N^2) ti
                 return A[i];
         return N;
     }
-    //
-    public int findDuplicate(int[] A) {
+    //Approach 2 : Count frequency of each | O(N) time, O(N) space, boolean Array, HashMap, HashSet
+    public int findDuplicate_boolean_Array(int[] A) {
         int N = A.length;
         boolean[] visited = new boolean[N+1];
         for(int i=0;i<N;i++){
@@ -24,5 +25,16 @@ Approach 3 : Brute Force, Use two loops, Search each element in array, O(N^2) ti
         }
         return N;
     }
-
+//Approach 4 : O(1) space | Mark Visited by Marking values as -ve | Initially all values are +ve
+    public int findDuplicate(int[] A) {
+        int N = A.length;
+        System.out.println(Arrays.toString(A));
+        for(int i=0;i<N;i++){
+            int curr = Math.abs( A[i] );
+            if( A[ curr-1 ] < 0 )
+                return curr;
+            A[ curr-1 ] = A[ curr-1 ] * -1;
+        }
+        return N;
+    }
 }
