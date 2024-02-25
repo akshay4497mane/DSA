6843460b4f8d3c48f42b085de4eae1c962552368
Akshay Mane <akshay4497mane@gmail.com>
2024-02-25 18:14:52 +0530

    Time: 9 ms (65.57%) | Memory: 57.2 MB (40.41%) - LeetSync

--- 878-shifting-letters/shifting-letters.java
+++ 878-shifting-letters/shifting-letters.java
@@ -1,4 +1,9 @@
 class Solution {
+/*
+1st pass: No of shifts required can be precalculated in one array traversal.
+2nd pass : compute shifting of characters 
+Need to handle modulo 26 for overflows in both loops
+*/
     public String shiftingLetters(String s, int[] shifts) {
         StringBuilder sb = new StringBuilder();
         for(int i = shifts.length-2; i>=0 ; i--){
@@ -9,5 +14,21 @@ class Solution {
             sb.append((char) ((( (s.charAt(i)-'a') + shifts[i]) % 26 ) + 'a') );
         }
         return sb.toString();
+    }
+}
+
+class Solution2 {
+    public String shiftingLetters(String s, int[] shifts) {
+        char p[]=s.toCharArray();
+        long n=0;
+        for(int i=0;i<shifts.length;i++)
+            n+=shifts[i];
+
+        for(int i=0;i<p.length;i++){
+            long ch=(p[i]-97+n)%26;
+            p[i]=(char)(97+ch);
+            n-=shifts[i];
+        }
+        return String.valueOf(p);
     }
 }
