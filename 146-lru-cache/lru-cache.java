9724795de724cf70b536d4ccab2836df6960b84a
Akshay Mane <akshay4497mane@gmail.com>
2024-02-25 23:57:10 +0530

    Time: 49 ms (50.38%) | Memory: 124.1 MB (17.66%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,8 +1,68 @@
-//Using inbuilt LinkedHashMap of Java
+//APPROACH 2 : Using HashMap + DLL
 class LRUCache {
+    class DLLNode{
+        int key, val;
+        DLLNode next, prev;
+        DLLNode(int k, int v, DLLNode n, DLLNode p){
+            key = k; val=v; next = n; prev = p;
+        }
+    }
+    int capacity;
+    Map<Integer, DLLNode> m;
+    DLLNode head, tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        m = new HashMap<>();
+        head = new DLLNode(-1, -1, null, null);
+        tail = new DLLNode(-1, -1, null, null);
+        head.next = tail;
+        tail.prev = head;
+    }
+    
+    public int get(int key) {
+        if( m.containsKey(key) ){
+            DLLNode node = m.get(key);
+            remove(node);
+            add(node);
+            return node.val;           
+        } 
+        return -1;
+    }
+    
+    public void put(int key, int val) {
+        if( m.containsKey(key) ){
+            DLLNode oldNode = m.get(key);
+            remove(oldNode);
+            //m.remove(key);
+        }
+        DLLNode node = new DLLNode(key, val, null, null);
+        m.put(key, node);
+        add(node);
+
+        if( m.size() > capacity ){
+            m.remove( head.next.key );
+            remove(head.next);
+        }
+    }
+    void add(DLLNode node){ //add in TAIL
+        node.prev = tail.prev;
+        node.next = tail;
+        tail.prev.next = node;
+        tail.prev = node;
+    }
+    void remove(DLLNode node){ //remove from HEAD
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+}
+
+
+//APPROACH 1 : Using inbuilt LinkedHashMap of Java
+class LRUCache_1 {
     int capacity;
     LinkedHashMap<Integer, Integer> lhm;
-    public LRUCache(int capacity) {
+    public LRUCache_1(int capacity) {
         this.capacity = capacity;
         lhm = new LinkedHashMap<>(5, 0.75f, true){
             @Override
@@ -16,8 +76,8 @@ class LRUCache {
         return lhm.getOrDefault(key, -1);
     }
     
-    public void put(int key, int value) {
-        lhm.put(key, value);        
+    public void put(int key, int val) {
+        lhm.put(key, val);        
     }
 }
 
@@ -25,5 +85,5 @@ class LRUCache {
  * Your LRUCache object will be instantiated and called as such:
  * LRUCache obj = new LRUCache(capacity);
  * int param_1 = obj.get(key);
- * obj.put(key,value);
+ * obj.put(key,val);
  */
