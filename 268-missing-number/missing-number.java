a81dac5e5202e2be9f66d15c2bc35f2203e09271
Akshay Mane <akshay4497mane@gmail.com>
2024-04-01 09:29:37 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (68.29%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,4 +1,21 @@
 class Solution {
+    //Approach 1 : Sort Nums array, ensure 0, last element, for i : ensure nums[i] == nums[i-1]
+        //Time : O( N logN ) | Space : O(1)
+    
+/* Approach 2 : use HashSet | Time O(N) | Space : O(N)
+public int missingNumber(int[] nums) {
+    Set<Integer> numSet = new HashSet<>();
+    for(int num : nums) numSet.add(num);
+    int expectedCount = nums.length + 1;
+    for(int num = 0 ; num < expectedCount ; num++){
+        if(!numSet.contains(number))
+            return num;
+    }
+    return -1;
+}
+*/
+/*
+    //Approach 3 : using Boolean Array | Time:O(N), Space O(N)
     public int missingNumber(int[] nums) {
         int N = nums.length;
         boolean[] flag = new boolean[N+1];
@@ -11,5 +28,14 @@ class Solution {
                 return ind;
         }
         return ind;
+    }
+*/
+    //Approach 4 : Using gauss formula(sum of elements)
+    public int missingNumber(int[] nums) {
+        int N = nums.length;
+        int expectedSum = ( N * (N+1)) / 2;
+        int actualSum = 0;
+        for(int num : nums) actualSum += num;
+        return expectedSum - actualSum;
     }
 }
