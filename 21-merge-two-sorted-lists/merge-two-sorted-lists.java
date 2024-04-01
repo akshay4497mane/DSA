dcb4b60e2c52073df3a915967d77e38fdfd2e5cb
Akshay Mane <akshay4497mane@gmail.com>
2024-04-01 10:05:44 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (32.39%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -9,67 +9,23 @@
  * }
  */
 class Solution {
-    
-    //Approach 1: Recursive
-    public ListNode mergeTwoListsRECURSIVE(ListNode l1, ListNode l2){
-		if(l1 == null) return l2;
-		if(l2 == null) return l1;
-		if(l1.val < l2.val){
-			l1.next = mergeTwoLists(l1.next, l2);
-			return l1;
-		} else{
-			l2.next = mergeTwoLists(l1, l2.next);
-			return l2;
-		}
-    }
-    /*Approach 2: Non Recursive 
-    
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode ansHead= null, ans = null;
-        if(list1==null) return list2;
-        if(list2==null) return list1;
-                
-        if ( list1.val < list2.val ){
-            ans = list1;
-            list1 = list1.next;            
-        }
-        else{
-            ans = list2;
-            list2 = list2.next;            
-        }
-        ansHead = ans;
-        
-        while(list1 != null && list2!=null){
-            if(list1.val < list2.val ){
-                ans.next = list1;
-                list1 = list1.next;
+        ListNode i = list1, j = list2, ans = new ListNode(), dummyHead=ans;
+        while( i != null && j != null){
+            if(i.val < j.val){
+                ans.next = i;
+                i = i.next;
             }else{
-                ans.next = list2;
-                list2 = list2.next;                
+                ans.next = j;
+                j = j.next;
             }
             ans = ans.next;
         }
-        if(list1 != null ){
-            ans.next = list1;
+        if( i != null ){
+            ans.next = i;
         }else{
-            ans.next = list2;
+            ans.next = j;
         }
-        return ansHead;
-    }
-    */
-    //Approach 2: Non Recursive  + DUMMY NODE
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode prevAns, ans = new ListNode(0);
-        prevAns = ans;
-        while(list1 != null && list2!=null){
-            if(list1.val < list2.val ){
-                ans.next = list1;   list1 = list1.next;
-            }else{
-                ans.next = list2;   list2 = list2.next;                
-            }
-            ans = ans.next;
-        }
-        ans.next = ( list1 != null ) ? list1 : list2;
-        return prevAns.next;
+        return dummyHead.next;
     }
 }
