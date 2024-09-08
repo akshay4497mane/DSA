d30e2347603a8d7e44644c05c71936b7a8aa5d40
Akshay Mane <akshay4497mane@gmail.com>
2024-09-08 16:39:05 +0530

    Time: 6 ms (10.54%) | Memory: 41.6 MB (40.86%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -5,11 +5,25 @@ class Solution {
         Arrays.sort(arr, Collections.reverseOrder());
     */
     public int hIndex(int[] citations) {
+        Integer[] newArray = new Integer[citations.length];
+        int i = 0;
+        for (int value : citations) {
+            newArray[i++] = Integer.valueOf(value);
+        }
+
+        int N = newArray.length;
+        Arrays.sort( newArray, Collections.reverseOrder() );
+        for( i=0 ; i< N; i++ ){
+            if( newArray[i] <= i ) break;
+        }
+        return i;
+/* //Sorting Ascending Order :
         int i =1, N = citations.length;
         Arrays.sort( citations );
         for( i=1 ; i<= N; i++ ){
             if( citations[N-i] < i ) break;
         }
         return i-1;
+*/
     }
 }
