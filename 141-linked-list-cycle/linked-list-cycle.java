e1b990ac18c70c38529e926c322cde06105bfb94
Akshay Mane <akshay4497mane@gmail.com>
2024-09-08 23:55:07 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (38.89%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -10,6 +10,14 @@
  * }
  */
 public class Solution {
+
+    /*
+This approach uses Floyd's Cycle Detection Algorithm or the Tortoise and Hare Algorithm.
+We use two pointers: slow (one step at a time) and fast (two steps at a time).
+If the list has a cycle, the pointers will meet; otherwise, the fast pointer will reach the end.
+- Time Complexity: O(n)  
+- Space Complexity: O(1)
+*/
     public boolean hasCycle(ListNode head) {
         if ( head == null || head.next == null ) return false;
         ListNode slow = head.next, fast = head.next.next;
