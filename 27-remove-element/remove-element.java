09bc4e17aaaeec12742adfc0d83ae9d162af769f
Akshay Mane <akshay4497mane@gmail.com>
2024-09-08 17:27:13 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (94.17%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,10 +1,12 @@
 class Solution {
+/*
+The approach removes occurrences of val from the array by using two pointers: i moves from left to right, stopping at val, while j moves from right to left, stopping at non-val. When both pointers stop, the elements are swapped, and the process continues until i exceeds j. The function returns the new length of the array after removing val.
+*/
     public int removeElement(int[] A, int val) {
-        int i = 0; //moves left -> right | stops at finding val
         int N = A.length;
+        int i = 0; //moves left -> right | stops at finding val
         int j = N-1;//moves right-> left | stops at finding non val
         while( i <= j){
-            System.out.println("i= " + i + "A[i]:" + A[i] +  " j= " + j + "A[j] : " + A[j]);
             while( i < N && A[i] != val ){ i++; }
             while( j >= 0 && A[j] == val ){ j--; }
             if( i < j ){
@@ -17,30 +19,3 @@ class Solution {
         return j+1;        
     }
 }
-/*
-02 34 53 10 | val = 3
-i=2, j=7
-02 04 53 13 | i=3, j=6
-i=5, j=6
-02 04 51 33 | i=6, j=5
----
-Input
-nums =
-[1]
-val =
-1
-
-Use Testcase
-Output
-[1]
-Expected
-[]
-
------
-[0,1,2,2,3,0,4,2] | val = 2
-i=0, j=0 | swap
-i=1, j=0 | swap
-[1,0,2,2,3,0,4,2] | val = 2
-i=2, j=0 | 
-
-*/
