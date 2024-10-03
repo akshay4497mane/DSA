8b2340bbee4e1cb5d1c4de7e6b93cdcdde1fe647
Akshay Mane <akshay4497mane@gmail.com>
2024-10-04 03:20:25 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (89.85%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.java
+++ 226-invert-binary-tree/invert-binary-tree.java
@@ -15,12 +15,27 @@
  */
 class Solution {
     /* Recursion : Time: O(N), Space O(height) */
-    public TreeNode invertTree(TreeNode root) {
+    public TreeNode invertTree_Recursive(TreeNode root) {
         if( root == null ) return root;
         TreeNode newRight = invertTree(root.left);
         TreeNode newLeft = invertTree(root.right);
         root.left = newLeft;
         root.right = newRight;
+        return root;
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if( root == null ) return null;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add( root );
+        while( !q.isEmpty() ){
+            TreeNode curr = q.poll();
+            TreeNode temp = curr.left;
+            curr.left = curr.right;
+            curr.right = temp;
+            if(curr.left != null) q.add( curr.left );
+            if(curr.right != null) q.add( curr.right );
+        }
         return root;
     }
 }
