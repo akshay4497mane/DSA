08b88c13e4a4bd3edd7392bb155bb1ebe65df265
Akshay Mane <akshay4497mane@gmail.com>
2024-10-04 03:12:31 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (49.39%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.java
+++ 226-invert-binary-tree/invert-binary-tree.java
@@ -14,53 +14,12 @@
  * }
  */
 class Solution {
-    //Approach1: Recursive using Call Stack
-    public TreeNode invertTreeRecursive(TreeNode root) {
-        if(root == null) return root;
-        TreeNode temp = invertTree(root.left);
-        root.left = invertTree(root.right);
-        root.right = temp;
-        return root;
-    }
-    //Approach2: Iterative using Dynamic Heap Memory Stack
     public TreeNode invertTree(TreeNode root) {
-        if(root == null) return root;
-        Stack<TreeNode> st = new Stack<>();
-        //Use Dequeue<TreeNode> st = new ArrayDequeue<>();
-        //Use Dequeue<TreeNode> st = new LinkedList<>();
-        
-        st.push(root);
-        while( !st.isEmpty() ){
-            TreeNode node = st.pop();
-            TreeNode temp = node.left;
-            node.left = node.right;
-            node.right = temp;
-            if( node.left != null ) st.push(node.left);
-            if( node.right != null ) st.push(node.right);
-        }
+        if( root == null ) return root;
+        TreeNode newRight = invertTree(root.left);
+        TreeNode newLeft = invertTree(root.right);
+        root.left = newLeft;
+        root.right = newRight;
         return root;
     }
-    //Approach 3 : Use QUEUE  instead of stack | Level order to achieve same
-
-    /*
-    Recursion vs Stack Iteration 
-    RAM Memory vs HDD memory
-    Slow context switching vs Fast
-    https://stackoverflow.com/questions/35097729/recursion-vs-stack?rq=3
-    */
-
-    /* 
-    Java Stack which to use?  
-    Deque vs Stack
-    Interface vs Class
-    Not thread safe vs Thread safe
-    supports insert/delet both ends vs NA
-    NA vs st.get(1), st.put(1) modification risk as it extends vector
-    Iteration Top to bottom vs bottom to top
-
-    https://www.baeldung.com/java-deque-vs-stack
-
-    https://stackoverflow.com/questions/12524826/why-should-i-use-deque-over-stack
-
-    */
 }
