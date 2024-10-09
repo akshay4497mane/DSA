0f561ebf93a5e05c3481ab6dde0f274d3e029e07
Akshay Mane <akshay4497mane@gmail.com>
2024-10-09 23:13:14 +0530

    Time: 4 ms (11.63%) | Memory: 44.1 MB (6.15%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -1,21 +1,56 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
-    public int maxDepth(TreeNode root) {
-        if(root == null ) return 0;
+    /*
+    Max Depth = No of Nodes in longest path from root to leaf
+     * RECURSIVE DFS
+     * Time: O(N) visit each nodes once,
+     * SPACE : size of recursive stack = O(height) = best O(logN)/worst O(NN)
+     */
+    public int maxDepth_Recursive(TreeNode root) {
+        if (root == null)
+            return 0;
         return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
     }
+
+    /* Iterative DFS using Stack 
+    - Use Stack<TreeNode> for recursion
+    - Use Stack<Integer> to keep track of current depth
+    Time: O(N) / Space O(log N)
+    */
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        LinkedList<TreeNode> stackNodes = new LinkedList<>();
+        LinkedList<Integer> stackDepths = new LinkedList<>();
+        stackNodes.add(root); stackDepths.add(1);
+        int ansMaxDepth = 0, currDepth = 0;
+        TreeNode currNode = null;
+        while (!stackNodes.isEmpty()) {
+            currNode = stackNodes.pollLast();
+            currDepth = stackDepths.pollLast();
+            if (currNode != null) {
+                ansMaxDepth = Math.max(ansMaxDepth, currDepth);
+                stackNodes.add(currNode.left);
+                stackDepths.add(currDepth + 1);
+                stackNodes.add(currNode.right);
+                stackDepths.add(currDepth + 1);
+            }
+        }
+        return ansMaxDepth;
+    }
+
 }
