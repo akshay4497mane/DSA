5439d806ac3a4322ea105f8894e69146c6804976
Akshay Mane <akshay4497mane@gmail.com>
2025-03-07 02:31:13 +0530

    Time: 4 ms (9.53%) | Memory: 51.5 MB (33.30%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.java
+++ 975-range-sum-of-bst/range-sum-of-bst.java
@@ -1,16 +1,45 @@
 class Solution {
-    int ans;
+    /* 
+    DFS approach (Iterative):
+    - Uses a stack for traversal instead of recursion.
+    - Checks for null before processing or while inserting into stack.
+    - Avoids unnecessary traversal when node value is out of range.
     
+    Time Complexity: O(N) in worst case (skewed tree), O(log N) in balanced BST.
+    Space Complexity: O(H) for stack (H = height of BST).
+    */
     public int rangeSumBST(TreeNode root, int low, int high) {
-        ans = 0;
-        dfs(root, low, high);
+        int ans = 0;
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root); //no need for null check
+        
+        while (!st.isEmpty()) {
+            TreeNode node = st.pop();
+            if (node != null) { // Null check while processing
+                if (low <= node.val && node.val <= high) 
+                    ans += node.val;
+                
+                if (low < node.val) //search left
+                    st.push(node.left); //no need for null check
+                
+                if (high > node.val) //search right
+                    st.push(node.right); //no need for null check
+            }
+        }
         return ans;
     }
-    /* DFS approach : 
+
+    /* DFS Recursive approach : 
     Time Complexity: O(N) in worst case (skewed tree), O(log N) in balanced BST
     Space Complexity: O(H) for recursion stack (H = height of BST)
     Optimized: Avoids unnecessary traversal when value is out of range 
     */
+    int ans;
+    public int rangeSumBST2(TreeNode root, int low, int high) {
+        ans = 0;
+        dfs(root, low, high);
+        return ans;
+    }
     public void dfs(TreeNode root, int low, int high) {
         if (root != null) {
             if (low <= root.val && root.val <= high) 
