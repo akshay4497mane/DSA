ff0022f95e2fe5ad1cbf8261595186647bf6a956
Akshay Mane <akshay4497mane@gmail.com>
2025-03-16 22:27:04 +0530

    Time: 1 ms (8.36%) | Memory: 43 MB (6.70%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,10 +1,24 @@
 class Solution {
     int[][] DP =  new int[101][101];
-    public int uniquePaths(int m, int n) {
+    public int uniquePaths_Memoization(int m, int n) {
         if(m==0 || n==0) return 0;
         if(m==1 && n==1) return 1;
         if(DP[m][n] != 0) return DP[m][n];
         DP[m][n] = uniquePaths(m-1,n) + uniquePaths(m,n-1);
         return DP[m][n];
+    }
+
+    public int uniquePaths(int m, int n) {
+        for(int i=0; i<n ; i++)
+            DP[0][i] = 1;
+        for(int i=0; i<m ; i++)
+            DP[i][0] = 1;
+
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                DP[i][j] = DP[i-1][j] + DP[i][j-1];    
+            }
+        }
+        return DP[m-1][n-1];
     }
 }
