a48978f0081eede1c150f6529e25a70f6e873f0c
Akshay Mane <akshay4497mane@gmail.com>
2025-03-22 15:32:20 +0530

    Time: 1 ms (99.99%) | Memory: 57.4 MB (38.71%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -3,7 +3,7 @@
 
 
 class Solution {
-    // Using boolean array of n size
+    // Approach 1 : Using boolean array of n size
     // Time Complexity: O(n) - Single pass to mark seen numbers, single pass to find missing.
     // Space Complexity: O(n) - Extra boolean array of size n+1.
     public int firstMissingPositive_booleanArray(int[] nums) {
@@ -32,12 +32,14 @@ class Solution {
         // If all 1 to n are present, next positive (n+1) is the answer
         return n + 1;
     }
-    /* 
-       Problem: Find the smallest missing positive integer in an unsorted array.
+    /* APPROACH 2 :
+        1. Normalize the array by replacing invalid values (≤0 or >n) with 1 and check if 1 exists.  
+        2. Use array indices as a hash to mark the presence of numbers.  
+        3. Find the first missing positive by checking unmarked indices; return `n + 1` if all are present.
        Time Complexity: O(n) (Single pass modifications + One pass marking + One pass checking)
        Space Complexity: O(1) (In-place modification of the input array)
     */
-    public int firstMissingPositive(int[] nums) {
+    public int firstMissingPositive_approach2(int[] nums) {
         int n = nums.length;
         boolean seen1 = false;
         
@@ -69,5 +71,28 @@ class Solution {
         if (nums[0] > 0) return n; 
         
         return n + 1; // If all 1 to n exist, return n+1
+    }
+    /* Approach 3 : Cycle Sort 
+    */
+    public int firstMissingPositive(int[] A) {
+        int n = A.length;
+        int i = 0;
+        while(i < n ){
+            int correctIndex = A[i]-1;
+            if( A[i]>0 && A[i]<=n && A[i] != A[correctIndex] ){
+                swap(A, i, correctIndex);
+            }else{
+                i++;
+            }
+        }
+        for(int j=0; j<n; j++){
+            if(j+1 != A[j]) return j+1;
+        }
+        return n+1;
+    }
+    void swap(int[] A, int p, int q){
+        int temp = A[p];
+        A[p] = A[q];
+        A[q] = temp;
     }
 }
