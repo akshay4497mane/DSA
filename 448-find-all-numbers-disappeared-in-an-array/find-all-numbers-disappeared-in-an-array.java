adf2e6159d0439352ba9ceb9682c03e7bf21de75
Akshay Mane <akshay4497mane@gmail.com>
2025-03-22 16:04:15 +0530

    Time: 20 ms (33.67%) | Memory: 55.7 MB (98.13%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
@@ -1,24 +1,36 @@
 /*  
- * Problem: Find all missing numbers in the range [1, n] from an unsorted array of size n.  
- *  
- * Time Complexity: O(n)  
- * - Each element is swapped at most once, leading to an O(n) cycle sort process.  
- * - The final pass to collect missing numbers is O(n), keeping the overall complexity O(n).  
- *  
- * Space Complexity: O(1)  
- * - Sorting is done in place without extra space (excluding output list).  
+ * Problem: Find All Numbers Disappeared in an Array | Find all missing numbers in the range [1, n] from an unsorted array of size n.Input: nums = [4,3,2,7,8,2,3,1] Output: [5,6]  
  */
 
 class Solution {
-    
+    /* Approach 1 : using HashMap
+    */
+    public List<Integer> findDisappearedNumbers(int[] A) {
+        int n = A.length;
+        Map<Integer, Boolean> m = new HashMap<>();
+        for(int i=0; i<n; i++){
+            m.put(A[i], true);
+        }
+        List<Integer> ans = new ArrayList<>();
+        for(int i=1; i<=n; i++){
+            if(!m.containsKey(i)) 
+                ans.add(i);
+        }
+        return ans;
+    }
     /*  
-     * Approach: Cycle Sort  
+     * Approach 2: Cycle Sort  
+        * Time Complexity: O(n)  
+        * - Each element is swapped at most once, leading to an O(n) cycle sort process.  
+        * - The final pass to collect missing numbers is O(n), keeping the overall complexity O(n).  
+        *  
+        * Space Complexity: O(1)  
+        * - Sorting is done in place without extra space (excluding output list).  
      * - Every element should be at index `A[i] - 1` if it's in the range [1, n].  
      * - Swap elements into correct positions.  
      * - The numbers that remain misplaced indicate missing numbers.  
      */
-    
-    public List<Integer> findDisappearedNumbers(int[] A) {
+    public List<Integer> findDisappearedNumbers_Approach2(int[] A) {
         int i = 0, n = A.length;
 
         // Place each number at its correct index using cycle sort
