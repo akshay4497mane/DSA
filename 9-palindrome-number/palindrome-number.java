eb8df5215eb520af83ffa3467cef07487f4abe5e
Akshay Mane <akshay4497mane@gmail.com>
2025-04-03 03:50:44 +0530

    Time: 4 ms (100.00%) | Memory: 44.6 MB (7.87%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,5 +1,22 @@
 class Solution {
+    /* Approach 1: Find low digits using %,/ and Convert num to string, compare with 2 pointers
+    Time: O(N), Space : O(N)
+    Approach 2 : Find low digits by %/, convert num into ReverseNumber, But it may lead to overflow
+    Approach 3: Find low dig and convert only half of the integer
+     */
     public boolean isPalindrome(int x) {
+        if(x<0) return false;//handle -ve case
+        if(x%10 == 0 && x!=0) return false; //handle 132000 case | reverse 231
+        int reverseX = 0;
+        while( reverseX < x ){
+            reverseX = reverseX * 10 + (x%10);
+            x = x/10;
+        }
+        if( x==reverseX) return true;//123321 => 123 == 321
+        if( x==reverseX/10) return true; // 12321 => 12 == 123/10
+        return false;
+    }
+    public boolean isPalindrome_approach1(int x) {
         if(x<0) return false;
         StringBuilder sb = new StringBuilder();
         while(x > 0){
