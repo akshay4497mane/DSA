cc17c34124db069189b4df4c6b16d1987dfa53e4
Akshay Mane <akshay4497mane@gmail.com>
2025-04-06 02:43:27 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (6.10%) - LeetSync

--- 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.java
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.java
@@ -3,17 +3,54 @@ Problem: Find the lowest common ancestor (LCA) of the deepest leaves in a binary
 
 Sample Input: [1,2,3,4,5,null,6,null,null,7]
 Sample Output: Node with value 2
-
-Approach: 
-- Use level-order traversal to get the leftmost and rightmost nodes at the deepest level.
-- Then find the LCA of those two nodes.
-
-Time Complexity: O(N)
-Space Complexity: O(H)
 */
 
 class Solution {
+/*
+Approach 2: ( DFS )
+Using 1-pass DFS.
+For each subtree, return a pair of (depth, LCA node). Recurse and compare depths.
+Time: O(N) - visit each node once
+Space: O(H) - recursion stack space, H = height of tree
+
+*/
     public TreeNode lcaDeepestLeaves(TreeNode root) {
+        return dfs(root).LCANode; // Return LCA node from result pair
+    }
+    private PairLCA dfs(TreeNode root) {
+        if (root == null) return new PairLCA(root, 0); // Base case: empty node has depth 0, RCA null
+
+        PairLCA leftAns = dfs(root.left); // Recurse into left subtree
+        PairLCA rightAns = dfs(root.right); // Recurse into right subtree
+
+        if (leftAns.depth > rightAns.depth) // Left subtree is deeper
+            return new PairLCA(leftAns.LCANode, leftAns.depth + 1); // Propagate deeper LCA up with updated depth
+
+        if (leftAns.depth < rightAns.depth) // Right subtree is deeper
+            return new PairLCA(rightAns.LCANode, rightAns.depth + 1); // Propagate deeper LCA up with updated depth
+
+        return new PairLCA(root, leftAns.depth + 1); // Equal depth → current root node is LCA
+    }
+    private class PairLCA {
+        /*Alternative approach : Pair<TreeNode, Integer> pair = new Pair<>(node, 0); 
+        pair.getKey() returns TreeNode | pair.getValue() returns int depth |
+        Cons: Less readabl
+        */
+        TreeNode LCANode; // LCA node at this subtree
+        int depth; // Max depth in this subtree
+        PairLCA(TreeNode lca, int dep) {
+            this.LCANode = lca; this.depth = dep;
+        }
+    }
+
+/*
+Approach 1 : ( BFS )
+- Use level-order traversal to get the leftmost and rightmost nodes at the deepest level.
+- Then find the LCA of those two nodes.
+Time Complexity: O(N)
+Space Complexity: O(H)
+*/
+    public TreeNode lcaDeepestLeaves_Approach1(TreeNode root) {
         Queue<TreeNode> q = new ArrayDeque<>(); // Queue for BFS
         q.add(root);
 
