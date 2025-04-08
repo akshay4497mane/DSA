a6d4966de35af52e489fffea7f91ef69d52ba325
Akshay Mane <akshay4497mane@gmail.com>
2025-04-09 02:09:42 +0530

    Time: 6 ms (76.14%) | Memory: 45 MB (83.46%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,37 +1,36 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        //construct graph  bi->ai
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        for(int i=0; i < numCourses; i++){
-            graph.add( new ArrayList<>() );
+        List<List<Integer>> graph = new ArrayList<>();
+        for(int i=0;i<numCourses; i++){
+            graph.add( new ArrayList<Integer>() );
         }
-        int[] indegree = new int[numCourses]; //IN DEGREE for all Nodes
-        for(int i=0; i < prerequisites.length; i++){
-            int v = prerequisites[i][0];
-            int u = prerequisites[i][1];
-            graph.get(u).add(v); // u -> v   [u is prerequisite for v]
-            indegree[v]++; //Pre Calculating IN DEGREE while constructing Graph
+        int[] indegree = new int[numCourses];
+        for(int[] pair : prerequisites){
+            graph.get(pair[1]).add(pair[0]);
+            indegree[pair[0]]++;
         }
-        //find cycle | KAHNS ALGORITHM using indegree[]
-        int topoLength = 0; //Topological Sort : v1 -> v2 -> v3...
-        Queue<Integer> q = new LinkedList<>();
-        for(int i=0; i < numCourses; i++){
+
+        Queue<Integer> q = new ArrayDeque<>();
+        for(int i=0; i<numCourses; i++){
             if(indegree[i] == 0)
-                q.add( i );
+                q.add(i);
         }
-        while( q.size() > 0 ){
-            int rem = q.remove();
-            topoLength++;
-            for ( int v : graph.get(rem)){
-                indegree[v]--;
-                if( indegree[v] == 0 )
-                    q.add(v);
+        if(q.size()==0) return false; //No nodes with indegree=0 => no course to start with
+
+        //Kahn's Algorithm to find cycle
+        int nodesVisited = 0;
+        while(!q.isEmpty()){
+            int node = q.poll();
+            nodesVisited++;
+            for(int neighbour : graph.get(node)){
+                indegree[neighbour]--;
+                if(indegree[neighbour] == 0)
+                    q.add(neighbour);
             }
         }
-        if( topoLength == numCourses )//no Cycle
+        if(nodesVisited == numCourses)
             return true;
         else
-            return false;
+            return false; //cycle
     }
 }
-//50:31
