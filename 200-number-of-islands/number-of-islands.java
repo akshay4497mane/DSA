fce4ffd15dfe9eb1008864ede54dad44fc1e3307
Akshay Mane <akshay4497mane@gmail.com>
2025-04-12 02:17:54 +0530

    Time: 4 ms (49.68%) | Memory: 49.5 MB (70.85%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,29 +1,28 @@
 class Solution {
-    int y; //height
-    int x; //width
-    char[][] g; // grid stored to reduce memory usage(pevents copies during recursion)
+        int[][] dir = {{-1, 1, 0, 0 }, 
+                       { 0, 0, 1, -1}};
     public int numIslands(char[][] grid) {
-        g = grid;
-        int ans = 0; //count of no if islands
-        y = grid.length;  
-        if(y == 0 ) return 0;
-        x = grid[0].length;
-        for(int i=0; i<y ; i++){
-            for(int j=0; j<x; j++){
-                if(g[i][j] == '1' ){
-                    dfs(i,j);
-                    ans++;
+        int numOfIslands = 0;
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
+                if(grid[i][j]=='1'){
+                    dfs(grid, i, j);
+                    numOfIslands++;
                 }
             }
         }
-        return ans;
+        return numOfIslands;
     }
-    public void dfs(int i, int j){
-        if(i<0 || j<0 || i>=y || j>=x || g[i][j]!='1') return;
-        g[i][j] = '0'; //mark visited
-        dfs(i-1, j );
-        dfs(i+1, j );
-        dfs( i, j-1 );
-        dfs( i, j+1 );
+    void dfs(char[][] grid, int r, int c){
+        //check boundary | 0 condition
+        if(r<0 || c<0 || r>=grid.length || c>= grid[0].length || grid[r][c]=='0'){
+            return;
+        }
+        grid[r][c]='0';
+        for(int i=0; i<dir[0].length; i++){
+            int R = r + dir[0][i];
+            int C = c + dir[1][i];
+            dfs( grid, R, C);
+        }
     }
 }
