6a148db6599b40e20cfa7ca6c27604e56f556d93
Akshay Mane <akshay4497mane@gmail.com>
2025-04-12 15:59:20 +0530

    Time: 1 ms (99.82%) | Memory: 61.5 MB (82.35%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,25 +1,13 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-        /* Approach1: Brute Force Approach |try all pairs i,j | O(N^2)
-        for buy 0:N-1
-            for sell buy:N-1
-                find max profit
-        Approach2: O(N) | maintain buyi = 0, selli = -1, maxProfit = 0
-        */
-        int buyi=0, buyVal=prices[0];
-        int selli, sellVal, currProfit, maxProfit =0;
-
-        for(int i=1;i<prices.length ;i++){
-            if( prices[i] < prices[buyi] )
-                buyi = i;
-            else{//sell
-                currProfit = prices[i]-prices[buyi];
-                if( currProfit > maxProfit){
-                    maxProfit = currProfit;
-                    selli = i;
-                }
+    public int maxProfit(int[] prices) {     
+        int profit =0, buyPrice=prices[0];
+        for(int i=1; i<prices.length; i++){
+            if(prices[i]>buyPrice){
+                profit = Math.max(profit, prices[i]-buyPrice);
+            }else{
+                buyPrice = prices[i];
             }
         }
-        return maxProfit;
+        return profit;
     }
 }
