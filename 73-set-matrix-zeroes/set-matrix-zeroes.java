e252db859d730d881c7e89f21d40dbe30458b0af
Akshay Mane <akshay4497mane@gmail.com>
2025-06-10 09:33:48 +0530

    Time: 0 ms (100.00%) | Memory: 46.1 MB (7.89%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,27 +1,51 @@
 class Solution {
-    public void setZeroes(int[][] mat) {
-        boolean flag0Row = false, flag0Col=false;
-        for(int i=0;i<mat.length;i++){
-            for(int j=0;j<mat[0].length; j++){
-                if(mat[i][j]==0){
-                    mat[i][0] = 0;
-                    mat[0][j] = 0;
-                    if(i==0) flag0Row = true;
-                    if(j==0) flag0Col = true;
+    public void setZeroes(int[][] A) {
+        int R = A.length;
+        int C = A[0].length;
+        Boolean isFirstCol = false;
+        for(int i=0; i<A.length; i++){
+            if(A[i][0]==0)
+                isFirstCol = true;
+            for(int j=1; j<A[0].length; j++){
+                if(A[i][j]==0){
+                    A[i][0] = 0;
+                    A[0][j] = 0;
+                }                
+            }
+        }
+
+        //process first column of every row for every 0
+        for(int i=1; i<A.length; i++){
+            if(A[i][0]==0){
+                for(int j=1; j<A[0].length; j++){
+                    A[i][j] = 0;
                 }
             }
         }
-        for(int i=1;i<mat.length;i++){
-            for(int j=1;j<mat[0].length; j++){
-                if(mat[i][0]==0 || mat[0][j]==0)
-                    mat[i][j] = 0;
+        //process first row of all columns for every 0
+        for(int j=1; j<A[0].length; j++){
+            if(A[0][j]==0){
+                for(int i=1; i<A.length; i++){
+                    A[i][j] = 0;
+                }
             }
         }
-        if( flag0Row == true)
-            for(int j=0; j<mat[0].length; j++)
-                mat[0][j] = 0;
-        if( flag0Col == true)
-            for(int i=0; i<mat.length; i++)
-                mat[i][0] = 0;
+        if(A[0][0]==0){//First row is zero
+            for(int j=1; j<A[0].length; j++)
+                A[0][j] = 0;   
+        }
+        if(isFirstCol)
+            for(int i=0; i<A.length; i++)
+                A[i][0] = 0;
     }
 }
+/*
+Edge Case 1 : 
+[[1,0]]
+
+Edge Case 2: 
+    [1, 2, 3, 4],
+    [5, 0, 7, 8],
+    [0,10,11,12],
+    [13,14,15,0]
+*/
