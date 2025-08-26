6cd1983bac33f071bbe8e147ea4004760cfde42e
Akshay Mane <akshay4497mane@gmail.com>
2025-08-27 04:54:37 +0530

    Time: 1 ms (99.83%) | Memory: 61.8 MB (41.33%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,13 +1,26 @@
 class Solution {
-    public int maxProfit(int[] prices) {     
-        int profit =0, buyPrice=prices[0];
-        for(int i=1; i<prices.length; i++){
-            if(prices[i]>buyPrice){
-                profit = Math.max(profit, prices[i]-buyPrice);
-            }else{
-                buyPrice = prices[i];
+    //Brute Force | For each buy day, Find largest possible selling price | 
+    //Time : O(N^2)
+    public int maxProfit_1(int prices[]) {
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                int profit = prices[j] - prices[i];
+                if (profit > maxprofit) maxprofit = profit;
             }
         }
-        return profit;
+        return maxprofit;
+    }
+    //Time O(N) | single pass | keep track of Minimum BUY price so far | try all values greater than that continuously | when you find lesser value update min so far value.
+    public int maxProfit(int[] prices) {     
+        int maxProfit =0, minBuyPriceSoFar=prices[0];
+        for(int i=1; i<prices.length; i++){
+            if( prices[i] > minBuyPriceSoFar ){
+                maxProfit = Math.max(maxProfit, prices[i]-minBuyPriceSoFar);
+            }else{
+                minBuyPriceSoFar = prices[i];
+            }
+        }
+        return maxProfit;
     }
 }
