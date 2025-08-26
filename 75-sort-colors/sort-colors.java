93198562c139e2aa48b7cf88d2bf8348fd623a39
Akshay Mane <akshay4497mane@gmail.com>
2025-08-27 00:35:21 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (66.53%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,5 +1,29 @@
 class Solution {
     public void sortColors(int[] nums) {
+        int p0 =0, curr =0;
+        int p2=nums.length-1;
+        int tmp;
+        while(curr <= p2){
+            if(nums[curr]==0){
+                //swap p0, curr
+                tmp = nums[p0];
+                nums[p0++] = nums[curr];
+                nums[curr++] = tmp;
+            }else if( nums[curr] == 2){
+                tmp = nums[curr];
+                nums[curr] = nums[p2];
+                nums[p2--] = tmp;
+            }else{
+                curr++;
+            }
+        }
+    }
+
+    /* The problem is known as Dutch National Flag Problem
+        Time : O(N) two passes
+        Space : O(1) | 3 int    
+    */
+    public void sortColors_2(int[] nums) {
         int [] index = new int[3];
         for( int i=0; i<nums.length; i++ ){
             index[nums[i]]++;
