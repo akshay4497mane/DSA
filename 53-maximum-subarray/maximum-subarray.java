b50049f9f93a737cf5093952cde0c96b0ce4c845
Akshay Mane <akshay4497mane@gmail.com>
2025-08-27 00:44:19 +0530

    Time: 1 ms (99.52%) | Memory: 57.5 MB (8.26%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,22 +1,40 @@
-class Solution {  // Kadane's Algorithm for finding the maximum subarray sum | O(N) Time complexity
-    public int maxSubArray(int[] A) {
-        int N = A.length; // Length of the input array
-        int maxSum,currSum; // Variable to store the maximum sum, current sum
-        // Base cases
-        if (N <= 0) return 0; // If the array is empty, return 0
-        if (N == 1) return A[0]; // If the array has only one element, return that element
-        // Initialize current sum and maximum sum with the first element of the array
-        currSum = maxSum = A[0];
-        // Traverse the array starting from the second element
-        for (int i = 1; i < N; i++) {
-            // Check if adding the current element improves the current subarray
-            if (A[i] > currSum + A[i]) { // Start a new subarray with the current element
-                currSum = A[i];
-            } else { // Include the current element in the existing subarray
-                currSum = currSum + A[i];
-            }
-            maxSum = Math.max(maxSum, currSum);// Update the maximum sum
+class Solution {
+/*
+//Kadane's Algorithm Return Maximum possible Sum for a Continuous SubArray
+Time : O(N) | space : O(1)
+*/
+    public int maxSubArray(int[] nums) {
+        int N = nums.length, currSum = 0, maxSum = Integer.MIN_VALUE;        
+        for(int i = 0; i<N ;i++){
+            currSum = Math.max( currSum + nums[i] , nums[i] );
+            maxSum = Math.max( currSum, maxSum);
         }
+        return maxSum;
+    }
+
+/*
+Approach : When we need to identify START / END element of maxim sum subarray
+Time : O(N) | space : O(1)
+*/
+    public int maxSubArray_2(int[] nums) {
+        int N = nums.length, currSum = 0, maxSum = Integer.MIN_VALUE;
+        int maxStart =0, maxEnd = 0, currStart = 0, currEnd = 0;
+        for(int i = 0; i < N ;i++){
+            if( currSum + nums[i] > nums[i] ){//Add nums[i]
+                currSum = currSum + nums[i]; currEnd = i;
+                if(currSum > maxSum){
+                    maxSum = currSum; maxStart = currStart; maxEnd = currEnd;
+                }
+            }else{//Dont add nums[i], start new subArray from nums[i]
+                currSum = nums[i]; currStart = currEnd = i;
+                if(currSum > maxSum){
+                    maxSum = currSum; maxStart = currStart; maxEnd = currEnd;
+                }
+            }
+        }
+        System.out.print("Max Sum SubARRAY: ");
+        for(int i= maxStart; i<= maxEnd; i++)
+            System.out.print(nums[i]);
         return maxSum;
     }
 }
