8867116db0e45134c1bb8020efbcc5f4e81dbe7e
Akshay Mane <akshay4497mane@gmail.com>
2025-08-26 23:26:30 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (5.16%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,43 +1,20 @@
 class Solution {
-    // Function to sort an array of colors in-place using DUTCH NATIONAL FLAG ALGORITHM
-    public void sortColors(int[] A) {// Time Complexity: O(N), Space Complexity: O(1)
-        int N = A.length, p1 = 0, p2 = N - 1, i = 0;
-        if (N <= 1)
-            return;
-        while (i <= p2) {
-            if (A[i] == 0) {
-                swap(A, i, p1);
-                i++;
-                p1++;
-            } else if (A[i] == 2) {
-                swap(A, i, p2);
-                p2--; //not incrementing i
-            } else { // A[i] == 1
-                i++;
-            }
+    public void sortColors(int[] nums) {
+        int [] index = new int[3];
+        for( int i=0; i<nums.length; i++ ){
+            index[nums[i]]++;
         }
-    }
-    private void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
+        int i = 0;
+        for(int j=0; j<index[0]; j++){
+            nums[i++] = 0;
+        }
 
-    //Sort an array of colors using COUNTING SORT
-    // Time Complexity: O(N), Space Complexity: O(numColors)
-    public void sortColors2PassCountingSort(int[] A) {
-        int numColors = 3; // Number of colors in the input array
-        int[] count = new int[numColors];
-        int N = A.length; // Length of the input array
-        
-        for (int i = 0; i < N; i++)
-            count[A[i]]++; // Count the occurrences of each color
+        for(int j=0; j<index[1]; j++){
+            nums[i++] = 1;
+        }
 
-        int base = 0; // Index to update the sorted array
-        for (int x = 0; x < numColors; x++) {// Update the array with sorted colors based on the count
-            for (int k = 0; k < count[x]; k++)
-                A[base + k] = x;
-            base += count[x];
+        for(int j=0; j<index[2]; j++){
+            nums[i++] = 2;
         }
     }
 }
