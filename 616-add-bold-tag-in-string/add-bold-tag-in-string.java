429ab5c08abd993d28414492039123dcc5fee8f3
Akshay Mane <akshay4497mane@gmail.com>
2025-08-28 05:32:46 +0530

    Time: 5 ms (96.58%) | Memory: 44.5 MB (74.36%) - LeetSync

--- 616-add-bold-tag-in-string/add-bold-tag-in-string.java
+++ 616-add-bold-tag-in-string/add-bold-tag-in-string.java
@@ -1,4 +1,19 @@
 class Solution {
+/* 
+Time complexity: 
+To calculate bold, we iterate over words. 
+For each word, we use indexOf() which costs O(n⋅k). 
+However, we may call it multiple times per word. 
+In the worst case scenario, such as s = "aaaaa...aaaaa" and word = "aaaaaa", it may be called O(n−k) times.
+Each word could cost us O((n−k)⋅n⋅k)
+
+There are m words, which means calculating bold could cost 
+O(m * (n−k) *  n*k)
+
+After calculating bold, we create the answer in O(n). This work is dominated by the other terms.
+
+Space complexity: O(n) | We use the boolean array bold which has a length of n.
+*/
     public String addBoldTag(String s, String[] words) {
         int n = s.length();
         boolean[] bold = new boolean[n];
