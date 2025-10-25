d9aa27a415f91f7d10510dde21e967a645560dff
Akshay Mane <akshay4497mane@gmail.com>
2025-10-26 02:32:40 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (62.21%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -4,7 +4,11 @@ Recurrence :  rob[n] = max( arr[n] + rob[n-2], rob[n-1] ).
 ARR   : 1 2 3 1
 Cache : 1 2 4 4    
 */
-    public int rob(int[] nums) {
+
+/*Approach 1 : Bottom Up DP | Filling DP Array from Left to Right |O(N) space/ O(N) Time 
+Recurrence: dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+*/
+    public int rob_1(int[] nums) {
         int n = nums.length;
         if(n==0) return 0;
         if(n==1) return nums[0];
@@ -16,5 +20,22 @@ Cache : 1 2 4 4
             cache[i] = Math.max( nums[i] + cache[i-2], cache[i-1] );
         }
         return cache[n-1];
+    }
+
+/*Approach 2 : Same as apprach 1, O(1) space */
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+
+        int prev2 = nums[0];                       // dp[i-2]
+        int prev1 = Math.max(nums[0], nums[1]);    // dp[i-1]
+
+        for (int i = 2; i < n; i++) {
+            int curr = Math.max(nums[i] + prev2, prev1);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
     }
 }
