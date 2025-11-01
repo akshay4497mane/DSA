a63a86948011f93b4d6b7a4104276ace47a76003
Akshay Mane <akshay4497mane@gmail.com>
2025-11-02 03:59:08 +0530

    Time: 21 ms (46.94%) | Memory: 178.3 MB (5.60%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
@@ -9,7 +9,7 @@
  * }
  */
 class Solution {
-    public ListNode modifiedList(int[] nums, ListNode head) {
+    public ListNode modifiedList_1(int[] nums, ListNode head) {
         Set<Integer> numSet = new HashSet<>();
         for(int num : nums){
             numSet.add(num);
@@ -27,5 +27,27 @@ class Solution {
             }
         }
         return headBackup;
+    }
+    /*Approach 2 : Using DUMMY Head
+    Time complexity: O(m+n)
+    Space complexity: O(m)
+    */
+    public ListNode modifiedList(int[] nums, ListNode head) {
+        Set<Integer> numSet = new HashSet<>();
+        for (int num : nums) numSet.add(num);
+        
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode curr = head, prev = dummy;
+
+        while (curr != null) {
+            if (numSet.contains(curr.val)) {
+                prev.next = curr.next; // remove node
+            } else {
+                prev = curr; // move forward
+            }
+            curr = curr.next;
+        }
+        return dummy.next;
     }
 }
