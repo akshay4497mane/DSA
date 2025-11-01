6b6d4267e31b0beea0411bb9e2681af2a96945e1
Akshay Mane <akshay4497mane@gmail.com>
2025-11-02 05:24:45 +0530

    Time: 1399 ms (6.30%) | Memory: 55.9 MB (8.86%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,8 +1,23 @@
 class Solution {
+//majority element — the number that appears more than n/2 times in an array.
 /*
-Boyer–Moore Voting Algorithm
-majority element — the number that appears more than n/2 times in an array.
-
+Approach 1: Brute force | For each num | count occurrences and return if > n/2
+O(N^2)
+*/
+    public int majorityElement(int[] A) {
+        int majority = A.length/2;
+        for(int num : A){
+            int count =0;
+            for(int ele : A){
+                if(num == ele) count++;
+            }
+            if(count > majority)
+                return num;
+        }
+        return -1;
+    }
+/*
+Approach : Boyer–Moore Voting Algorithm | Time: O(N), Space : O(1)
 It works because of vote cancellation logic (Boyer–Moore algorithm):
 You keep a candidate (x) and a count (xCount).
 -> When the same number appears, you increase the count.
@@ -12,7 +27,7 @@ Each different element cancels one vote from the current candidate.
 Since the majority element appears more than half the time, it can’t be fully canceled — it will always remain as the final candidate.
 Result → the last surviving x is the majority element.
 */
-    public int majorityElement(int[] A) {
+    public int majorityElement_2(int[] A) {
         int candidate = 0, count = 0;
         for (int num : A) {
             if (count == 0) 
@@ -21,7 +36,6 @@ Result → the last surviving x is the majority element.
         }
         return candidate;
     }
-
     public int majorityElement_1(int[] A) {
         int x = 0, xCount = 0;
         for(int i=0; i<A.length; i++){
