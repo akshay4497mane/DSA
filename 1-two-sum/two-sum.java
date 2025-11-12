b61a40b0a20baf8a80ecd3f21fc68143bfcadfb7
Akshay Mane <akshay4497mane@gmail.com>
2025-11-12 13:10:20 +0530

    Time: 4 ms (54.44%) | Memory: 46.6 MB (8.35%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,32 +1,81 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            map.put(nums[i], i);
+        }
+        for(int i=0; i<nums.length; i++){
+            if(map.containsKey(target-nums[i]) && map.get(target-nums[i])!=i){
+                return new int[]{ i, map.get(target-nums[i]) };
+            }
+        }
+        return new int[2];
+    }
 /*
-Problem: Find two indices i, j in an array such that A[i] + A[j] = target sum.
-Sample Input: A = [2, 7, 11, 15], sumNeeded = 9
-Sample Output: [0, 1] (Since 2 + 7 = 9)
+    Given two integers, describe an algorithm to determine 
+if both numbers contain the same digits. 
+The number of occurrences don't matter. 
+For example, 
+1234 and 
+4444311111222 contain the same digits, 
+but 1234 and 5432 don't.
 
-Alternative Approaches:
-1. Brute Force: Use two loops to check all pairs. O(N^2) time, O(1) space.
-2. Sorting + Binary Search: Sort array, use binary search for complement. O(N log N) time.
-3. HashMap: Store complements for O(1) lookup. O(N) time, O(N) space.
+
+num1
+num2
+
+1234  =>  1 , 2, 3, 4  ~ Map / Set
+123445
+*/
+/*
+    public int[] twoSum(int[] nums, int target) {
+        System.out.println( isSameDigit(1234, 12345));
+        return new int[3];
+    }
+//n1 = 12345, n2 = 1234
+set : 
+1235 / 1234444444
+
+// len1 ~ len2
+O(len1+len2)
+    boolean isSameDigit(int n1, int n2){    
+        Set<Integer> digits = new HashSet<>();
+        1->F # 2 F, 3 F, 4 F 5 t  
+
+        while(n1){
+            int curr = n1%10; //1234 => 4
+            digits.put(curr);
+            n1 = n1/10;
+        }
+        while(n2){
+            int curr = n2%10; //12345 => 5
+            if(NOT digits.contains(curr))
+                return false;
+            else
+               map.put(curr, false)
+            n2 = n2/10;
+        }    
+        //traverse map linearly FALSE   
+        //return false
+
+n : 11111111111111111111111111111111111111
+Hash(input) 
+input : 1 150 - 500 -1
+
+input -> hashcode ->  1-100 - index 0
+hashCode -> 101-200  - index 1
+
+akshay
+
+hashIndex = inputNum % sizeof;
+
+Array 100 elements
+
+1 -> 1 ->    ( O(N))
+ O(1)
+
+        return true;
+    }
 */
 
-import java.util.HashMap;
-import java.util.Map;
-
-class Solution {
-    public int[] twoSum(int[] A, int sumNeeded) {
-        int n = A.length;
-        Map<Integer, Integer> map = new HashMap<>(); // Stores value -> index
-        
-        for (int i = 0; i < n; i++) {
-            int target = sumNeeded - A[i]; // Compute required complement
-            
-            if (map.containsKey(target)) { // If complement exists, return indices
-                return new int[]{map.get(target), i};
-            }
-            
-            map.put(A[i], i); // Store current number and its index for future lookups
-        }
-        
-        return new int[]{-1, -1}; // Return invalid indices if no pair found
-    }
 }
