9f6735b353b3f19c1a42efd083a7a6b2d9497422
Akshay Mane <akshay4497mane@gmail.com>
2025-11-15 01:52:30 +0530

    Time: 2 ms (99.87%) | Memory: 45.7 MB (26.40%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,5 +1,16 @@
 class Solution {
-    public String minWindow(String s, String t) {
+    /*
+    Sliding Window L-R
+    jab ham right ko move kare tab hash[ch] if positive matlab dusari string me dala gaya tha => count++ => because current window me ans aa raha he
+    
+    when we find ch => frequency --
+    when we remove ch from window frequency++
+
+    Striver Video
+    https://www.youtube.com/watch?v=WJaij9ffOIY
+
+    */
+    public String minWindow_1(String s, String t) {
         int L=0, R=0, startIndex=-1, count = 0, minLen=Integer.MAX_VALUE;
         int[] hash = new int[256];
         for(char ch : t.toCharArray())
@@ -24,5 +35,41 @@ class Solution {
         }
         if(startIndex==-1) return "";
         return s.substring(startIndex, startIndex+minLen);
+    }
+
+
+    public String minWindow(String s, String t) {
+        int[] need = new int[256];
+        int missing = t.length();
+
+        // reverse meaning: we will decrement for T
+        for (char c : t.toCharArray()) need[c]--;
+
+        int L = 0, minLen = Integer.MAX_VALUE, start = 0;
+
+        for (int R = 0; R < s.length(); R++) {
+            char c = s.charAt(R);
+
+            // add: ++
+            if (need[c] < 0) missing--;
+            need[c]++;
+
+            // shrink
+            while (missing == 0) {
+                if (R - L + 1 < minLen) {
+                    minLen = R - L + 1;
+                    start = L;
+                }
+                char left = s.charAt(L);
+
+                // remove: --
+                need[left]--;
+                if (need[left] < 0) missing++;
+                L++;
+            }
+        }
+
+        if (minLen == Integer.MAX_VALUE) return "";
+        return s.substring(start, start + minLen);
     }
 }
