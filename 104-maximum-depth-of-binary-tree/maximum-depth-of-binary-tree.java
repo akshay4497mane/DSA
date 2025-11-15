2a36e9ecb0c74905f12e02cd9dbb311e56173b8a
Akshay Mane <akshay4497mane@gmail.com>
2025-11-15 23:31:44 +0530

    Time: 4 ms (0.94%) | Memory: 45.6 MB (8.00%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -14,11 +14,13 @@
  * }
  */
 class Solution {
+//15 Nov 2025
+
     /*
     Max Depth = No of Nodes in longest path from root to leaf
      * RECURSIVE DFS
      * Time: O(N) visit each nodes once,
-     * SPACE : size of recursive stack = O(height) = best O(logN)/worst O(NN)
+     * SPACE : size of recursive stack = O(height) = best O(logN)/worst O(N)
      */
     public int maxDepth_Recursive(TreeNode root) {
         if (root == null)
@@ -32,25 +34,45 @@ class Solution {
     Time: O(N) / Space O(log N)
     */
     public int maxDepth(TreeNode root) {
-        if (root == null)
-            return 0;
+        if (root == null) return 0;
         LinkedList<TreeNode> stackNodes = new LinkedList<>();
         LinkedList<Integer> stackDepths = new LinkedList<>();
         stackNodes.add(root); stackDepths.add(1);
-        int ansMaxDepth = 0, currDepth = 0;
-        TreeNode currNode = null;
+        int ansMaxDepth = 0, currDepth = 0; TreeNode currNode = null;
         while (!stackNodes.isEmpty()) {
-            currNode = stackNodes.pollLast();
-            currDepth = stackDepths.pollLast();
+            currNode = stackNodes.pollLast(); currDepth = stackDepths.pollLast();
             if (currNode != null) {
                 ansMaxDepth = Math.max(ansMaxDepth, currDepth);
-                stackNodes.add(currNode.left);
-                stackDepths.add(currDepth + 1);
-                stackNodes.add(currNode.right);
-                stackDepths.add(currDepth + 1);
+                stackNodes.add(currNode.left); stackDepths.add(currDepth + 1);
+                stackNodes.add(currNode.right); stackDepths.add(currDepth + 1);
             }
         }
         return ansMaxDepth;
     }
 
+/* Uses BFS to calculate the max depth of a binary tree by traversing level by level. 
+Algorithm:
+1. Initialize a queue, add the root node, and set depth to 0.
+2. While the queue is not empty, get the level size, remove nodes, and add their children.
+3. After processing all nodes at the current level, increment depth.
+4. Repeat until all levels are processed and return the depth.
+Time: O(N) / Space O(log N)
+    */
+    public int maxDepth_BFS(TreeNode root) {
+        if (root == null) return 0;
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.add(root);
+        int depth=0, levelSize =0;
+        TreeNode curr = null;
+        while(!q.isEmpty()){
+            levelSize = q.size();
+            for(int i=0; i<levelSize; i++){
+                curr = q.remove();
+                if( curr.left != null ) q.add(curr.left);
+                if( curr.right != null ) q.add(curr.right);
+            }
+            depth++;
+        }
+        return depth;
+    }
 }
