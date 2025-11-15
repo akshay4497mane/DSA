ec1b546c758debcf606d8b2bee83ad9cd9c578ac
Akshay Mane <akshay4497mane@gmail.com>
2025-11-15 21:28:56 +0530

    Time: 1 ms (7.07%) | Memory: 43 MB (12.66%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -14,7 +14,31 @@
  * }
  */
 class Solution {
+//Iterative
+/*
+Post : Left Righ Root
+PRE : Root Left Right | Iterative (print Root - push right - push left)
+
+Modified PRE for Post
+print Root - push left - push right  =>  Root - Rt - Left => Reverse -> Left Rt Root
+*/
     public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if(root==null) return ans;
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
+        while(!st.isEmpty()){
+            TreeNode curr = st.pop();
+            ans.add(curr.val);
+            if(curr.left!=null) st.push(curr.left);
+            if(curr.right!=null) st.push(curr.right);
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+
+//Recursive
+    public List<Integer> postorderTraversal_recursive(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
         post(root, ans);
         return ans;
