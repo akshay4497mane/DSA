1c7019e0314df59dfa9b0187efe430d4f3983a5b
Akshay Mane <akshay4497mane@gmail.com>
2025-11-15 22:52:03 +0530

    Time: 1 ms (90.11%) | Memory: 46.6 MB (15.61%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -14,21 +14,26 @@
  * }
  */
 class Solution {
+/*
+    approach 1 : While Loop till q isEmpty | Calculate size of queue as currLevelSize=> run FOR Loop | process children if Not Null
+    approach 2 : add Marker
+    approach 3 : Using 2 Queues
+*/
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
         if(root == null) return ans;
         Queue<TreeNode> q = new ArrayDeque<>();
         q.add(root);
         while(!q.isEmpty()){
-            int levelSize = q.size();
-            List<Integer> levelQ = new ArrayList<>();
-            for(int i=0; i<levelSize; i++){
+            int currLevelSize = q.size();
+            List<Integer> currLevelQ = new ArrayList<>();
+            for(int i=0; i<currLevelSize; i++){
                 TreeNode curr = q.remove();
-                levelQ.add(curr.val);
+                currLevelQ.add(curr.val);
                 if(curr.left!=null) q.add(curr.left);
                 if(curr.right!=null) q.add(curr.right);
             }
-            ans.add(levelQ);
+            ans.add(currLevelQ);
         }
         return ans;
     }
