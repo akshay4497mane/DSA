999ea59b9e2c93ef3ada2c15acd1f4221e814510
Akshay Mane <akshay4497mane@gmail.com>
2025-11-27 14:29:24 +0530

    Time: 1 ms (72.23%) | Memory: 43.5 MB (30.74%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -1,8 +1,69 @@
 class Solution {
+        public List<Integer> rightSideView(TreeNode root) {
+        if (root == null) return new ArrayList<Integer>();
+        ArrayDeque<TreeNode> queue = new ArrayDeque() {
+            {
+                offer(root);
+            }
+        };
+        List<Integer> rightside = new ArrayList();
+        while (!queue.isEmpty()) {
+            int levelLength = queue.size();
+            for (int i = 0; i < levelLength; ++i) {
+                TreeNode node = queue.poll();
+                if (i == levelLength - 1) {// if it's the rightmost element
+                    rightside.add(node.val);
+                }
+                if (node.left != null) 
+                    queue.offer(node.left);
+                if (node.right != null)
+                    queue.offer(node.right);
+            }
+        }
+        return rightside;
+    }
+
+    public List<Integer> rightSideView_12(TreeNode root) {
+        if (root == null) return new ArrayList<Integer>();
+        
+        ArrayDeque<TreeNode> nextLevel = new ArrayDeque() {{ offer(root); }};
+        ArrayDeque<TreeNode> currLevel = new ArrayDeque();        
+        List<Integer> rightside = new ArrayList();
+        
+        TreeNode node = null;
+        while (!nextLevel.isEmpty()) {
+            // prepare for the next level
+            currLevel = nextLevel;
+            nextLevel = new ArrayDeque<>();
+
+            while (! currLevel.isEmpty()) {
+                node = currLevel.poll();
+
+                // add child nodes of the current level
+                // in the queue for the next level
+                if (node.left != null) 
+                    nextLevel.offer(node.left);    
+                if (node.right != null) 
+                    nextLevel.offer(node.right);
+            }
+            
+            // The current level is finished.
+            // Its last element is the rightmost one.
+            if (currLevel.isEmpty()) 
+                rightside.add(node.val);    
+        }
+        return rightside;
+    }
+}
+
+class Solution_Approach2 {
 /*
 APPROACH 2 : 
-Step 1 : Level Order Traversal, 
+Step 1 : Level Order Traversal Using TWO Array LISTS
 Step 2 : Last element of every level => add it in ANS
+Time: O(N), Space: O(Diameter)
+
+Space: O(Height) for DFS, O(Width/Diameter) for BFS
 */
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> ansRightSide = new ArrayList<>();
@@ -61,3 +122,5 @@ Time: O(N), Space: O(H)
  *     }
  * }
  */
+
+ 
