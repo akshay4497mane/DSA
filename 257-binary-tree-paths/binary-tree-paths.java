f8d985bbe0506dee1c206b067bd5ccb820f44107
Akshay Mane <akshay4497mane@gmail.com>
2025-11-27 15:08:02 +0530

    Time: 1 ms (98.72%) | Memory: 43.7 MB (36.23%) - LeetSync

---
+++ 257-binary-tree-paths/binary-tree-paths.java
@@ -0,0 +1,21 @@
+class Solution {
+    List<String> ans = new ArrayList<>();
+    public List<String> binaryTreePaths(TreeNode root) {
+        helper(root, new StringBuilder());
+        return ans;
+    }
+
+    public void helper(TreeNode root, StringBuilder sb) {
+        if (root == null) return;
+        int len = sb.length();   // save state
+        sb.append(root.val);
+        if (root.left == null && root.right == null) {
+            ans.add(sb.toString());
+        } else {
+            sb.append("->");
+            helper(root.left, sb);
+            helper(root.right, sb);
+        }
+        sb.setLength(len);   // rollback to parent state
+    }
+}
