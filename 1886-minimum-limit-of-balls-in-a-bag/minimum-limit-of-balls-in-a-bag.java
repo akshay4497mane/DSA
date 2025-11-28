5146bea162d84e16d816f386bf51485c5ba75612
Akshay Mane <akshay4497mane@gmail.com>
2025-11-28 17:55:37 +0530

    Time: 86 ms (12.69%) | Memory: 70 MB (16.36%) - LeetSync

---
+++ 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.java
@@ -0,0 +1,31 @@
+class Solution {
+/*
+Approach: Binary Search
+Ans will lie between (positive)1...9(max of array)
+Video Aryan : https://www.youtube.com/watch?v=wIFX0quUQBw
+*/
+    public int minimumSize(int[] nums, int maxOperations) {
+        int left = 1, right = Arrays.stream(nums).max().getAsInt(), mid, ans=right;
+        //Integer[] arr = {5, 2, 9, 1, 7}; int max = Collections.max(Arrays.asList(arr));
+        //1...4...9
+        while( left<=right ){
+            mid = left + (right-left)/2;
+            if( isPossible(maxOperations, mid, nums) ){
+                ans=mid;
+                right = mid-1;
+            }else{
+                left=mid+1;
+            }
+        }
+        return ans;
+    }
+    Boolean isPossible(int maxOperations, int maxBallsInBag, int[] nums){
+        int operations = 0;
+        for(int num : nums){
+            operations += (int)Math.ceil( (double)num/maxBallsInBag ) -1;
+            if(operations> maxOperations)
+                return false;
+        }
+        return true;
+    }
+}
