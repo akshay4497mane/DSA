2bd1b1bc3b2ae06e43e0c0c6bfffed65c0c74337
Akshay Mane <akshay4497mane@gmail.com>
2025-12-01 20:46:24 +0530

    Time: 54 ms (89.82%) | Memory: 125.1 MB (51.37%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.java
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.java
@@ -14,17 +14,51 @@ Time complexity: O(M⋅logM+M⋅N)
         for( int[] meet : meetings ){
             int start = meet[0], end = meet[1];
             boolean foundUnusedRoomFlag = false;
+            for( int i=0; i<n ;i++ ){//for each rooms
+                if( roomFreeTime[i] <= start ){
+                    foundUnusedRoomFlag = true;
+                    meetCount[i]++;  roomFreeTime[i] = end;
+                    break;
+                }
+            }
+            if( !foundUnusedRoomFlag ){ //if NO room Found, Calculate wait time for EARLIEST/MINIMuM room
+                long minRoomFreeTime = Long.MAX_VALUE; int minRoomFreeIndex = 0; //if all rooms are busy, Find EARLIEST Free Room
+                for( int i=0; i<n ;i++ ){//for each rooms
+                    if( minRoomFreeTime > roomFreeTime[i] ){ //Find MINIMUM / earliest free room
+                        minRoomFreeTime = roomFreeTime[i]; minRoomFreeIndex = i;
+                    }
+                }                
+                roomFreeTime[minRoomFreeIndex] = minRoomFreeTime + end - start;
+                meetCount[minRoomFreeIndex] ++;
+            }
+        }
+        int maxMeetCount = 0, maxMeetCountRoomIndex = 0;
+        for(int i=0; i<n ; i++){
+            if( meetCount[i] > maxMeetCount ){
+                maxMeetCount = meetCount[i];
+                maxMeetCountRoomIndex = i;
+            }
+        }
+        return maxMeetCountRoomIndex;
+    }
+    //combined 2 loops
+    public int mostBooked_11(int n, int[][] meetings) {
+        long[] roomFreeTime = new long[n];//At what time each ROOM will be free?
+        int[] meetCount = new int[n]; //COUNT of meetings in every room
+        Arrays.sort(meetings, (a,b) -> a[0] - b[0] );//SORT based on START time
+
+        for( int[] meet : meetings ){
+            int start = meet[0], end = meet[1];
+            boolean foundUnusedRoomFlag = false;
             long minRoomFreeTime = Long.MAX_VALUE; int minRoomFreeIndex = 0; //if all rooms are busy, Find EARLIEST Free Room
             for( int i=0; i<n ;i++ ){//for each rooms
                 if( roomFreeTime[i] <= start ){
                     foundUnusedRoomFlag = true;
-                    meetCount[i]++;
-                    roomFreeTime[i] = end;
+                    meetCount[i]++;  roomFreeTime[i] = end;
                     break;
                 }else{
                     if( minRoomFreeTime > roomFreeTime[i] ){ //Find MINIMUM / earliest free room
-                        minRoomFreeTime = roomFreeTime[i];
-                        minRoomFreeIndex = i;
+                        minRoomFreeTime = roomFreeTime[i]; minRoomFreeIndex = i;
                     }
                 }
             }
