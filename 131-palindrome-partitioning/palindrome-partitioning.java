78be9532564787b908aec1076f41bd3c3b4157b6
Akshay Mane <akshay4497mane@gmail.com>
2025-12-01 19:55:32 +0530

    Time: 8 ms (56.18%) | Memory: 65.4 MB (15.10%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,4 +1,8 @@
-class Solution {
+class Solution_1 {
+    /**
+Time Complexity : O(N * 2^N), where N is the length of string s
+Hence, there could be 2^N possible substrings in the worst case. For each substring, it takes O(N) time to generate the substring and determine if it is a palindrome or not. This gives us a time complexity of O(N * 2^N))
+*/
     public List<List<String>> partition(String s) {
         List<List<String>> result = new ArrayList<List<String>>();
         dfs(0, new ArrayList<String>(), result, s);
@@ -21,5 +25,46 @@ class Solution {
                 return false;
         }
         return true;
+    }
+}
+
+class Solution{
+    private boolean[][] dp;          // constant reference
+    private String s;                // constant reference
+    private List<List<String>> res;  // constant reference
+
+    public List<List<String>> partition(String str) {
+        this.s = str;
+        int n = s.length();
+        this.dp = new boolean[n][n];
+        this.res = new ArrayList<>();
+
+        dfs(0, new ArrayList<>());
+        return res;
+    }
+
+    void dfs(int start, List<String> curr) {
+        if (start == s.length()) {
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+
+        for (int end = start; end < s.length(); end++) {
+            if (isPalindrome(start, end)) {
+                curr.add(s.substring(start, end + 1));
+                dfs(end + 1, curr);
+                curr.remove(curr.size() - 1);
+            }
+        }
+    }
+
+    boolean isPalindrome(int i, int j) {
+        if (s.charAt(i) == s.charAt(j) &&
+            (j - i <= 2 || dp[i + 1][j - 1])) {
+
+            dp[i][j] = true;   // only state mutation
+            return true;
+        }
+        return false;
     }
 }
