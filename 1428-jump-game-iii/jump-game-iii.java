344c251d4c34a3bec177ef3cf99d4d31fb4a8486
Akshay Mane <akshay4497mane@gmail.com>
2025-12-07 16:48:06 +0530

    Time: 9 ms (35.58%) | Memory: 48.3 MB (95.09%) - LeetSync

---
+++ 1428-jump-game-iii/jump-game-iii.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        int n = arr.length;
+        boolean[] visited = new boolean[n];
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start); visited[start] = true;
+        while (!q.isEmpty()){
+            int i = q.poll();
+            //visited[i]=true;
+            if (arr[i] == 0) return true; // If found zero → success
+            int forward = i + arr[i]; // Forward jump
+            if (forward < n && !visited[forward]) {
+                q.offer(forward); visited[forward] = true;
+            }
+            int backward = i - arr[i]; // Backward jump
+            if (backward >= 0 && !visited[backward]) {
+                q.offer(backward); visited[backward] = true;
+            }
+        }
+        return false;
+    }
+    //Without Boolean array | Use -1 * arr[i]  to mark visited
+    public boolean canReach_2(int[] arr, int start) {
+        int n = arr.length;
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            if (arr[node] == 0) // check if reach zero
+                return true;
+            if (arr[node] < 0)  //checking if already visited
+                continue;
+            // check available next steps
+            if (node + arr[node] < n) {
+                q.offer(node + arr[node]);
+            }
+            if (node - arr[node] >= 0) {
+                q.offer(node - arr[node]);
+            }
+            arr[node] = -arr[node];// mark as visited
+        }
+        return false;
+    }
+}
