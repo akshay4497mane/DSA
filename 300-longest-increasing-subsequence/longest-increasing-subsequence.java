933621e83b6548be016453135ef65904dd651673
Akshay Mane <akshay4497mane@gmail.com>
2025-12-10 01:54:57 +0530

    Time: 38 ms (64.43%) | Memory: 46 MB (59.57%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,5 +1,19 @@
 class Solution {
+    //nums = [0,1,0,3,2,3]
     public int lengthOfLIS(int[] nums) {
+        int N = nums.length, ans = 1;
+        int[] DP = new int[N]; Arrays.fill(DP, 1);
+        for(int i=1; i<N; i++){
+            for(int j=0; j<i; j++){ // Can we increase any of previous LIS answers using nums[i]
+                if(nums[i] > nums[j])
+                    DP[i] = Math.max(DP[i], DP[j]+1);
+            }
+            ans = Math.max(ans, DP[i]);
+        }
+        return ans;
+    }
+
+    public int lengthOfLIS_2(int[] nums) {
         int n=nums.length;
         int[] LIS = new int[n];
         int[] prevIndex = new int[n];
@@ -32,10 +46,11 @@ class Solution {
         Collections.reverse(ansArr);
         ansArr.stream().forEach(ele -> System.out.print(ele + " "));
         return ans;
-//Dry Run
-// [0,1,0,  3,2,3] | 
-//              i            
-//   LIS[1,2,1      3,3,4]
-//prevInd[-1,0,-1,  1,1,4]
+
+    //Dry Run
+    // [0,1,0,  3,2,3] | 
+    //              i            
+    //   LIS[1,2,1      3,3,4]
+    //prevInd[-1,0,-1,  1,1,4]
     }
 }
