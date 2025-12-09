0e3adc428d6cba6081ceff7f250370d76e5d5022
Akshay Mane <akshay4497mane@gmail.com>
2025-12-10 01:29:22 +0530

    Time: 35 ms (72.62%) | Memory: 64.1 MB (22.39%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -1,26 +1,32 @@
 class Solution {
 /*
-i=0 | j =n-1 (last)
-If s[i] == s[j], perform answer = 2 + LPS(i + 1, j - 1).
-Else, perform answer = max(LPS(i, j - 1), LPS(i + 1, j).
+Approach 1 :
+LPS using Recursion + Memoization
+Recurrence Relation
+    Keep 2 pointers i=0(start) | j =n-1 (last)
+    If s[i] == s[j], perform answer = 2 + LPS(i + 1, j - 1).
+    Else, perform answer = max(LPS(i, j - 1), LPS(i + 1, j).
+Time complexity: O(n^2)
+Space complexity: O(n^2)
 */
     String str;
     int[][] memo;
-    public int longestPalindromeSubseq(String s) {
+    public int longestPalindromeSubseq_1(String s) {
         str = s;
         memo = new int[s.length()][s.length()];
-        return lps(0, s.length()-1);
+        return LPS(0, s.length()-1);
     }
-    int lps(int i, int j){
+    int LPS(int i, int j){
         if(i>j) return 0;
         if(i==j) return 1;
         if(memo[i][j]!=0) return memo[i][j];
         if(str.charAt(i) == str.charAt(j))
-            return memo[i][j] = 2 + lps(i+1, j-1);
+            return memo[i][j] = 2 + LPS(i+1, j-1);
         else
-            return memo[i][j] = Math.max(lps(i, j-1), lps(i+1,j));
+            return memo[i][j] = Math.max(LPS(i, j-1), LPS(i+1,j));
     }
 
+    //Approach 2 : LPS(s) = LCS(s, reverse s) 
     //O(N^2)
     public int longestPalindromeSubseq_2(String s) {
         String revS = new StringBuilder(s).reverse().toString();
@@ -40,10 +46,37 @@ Else, perform answer = max(LPS(i, j - 1), LPS(i + 1, j).
         }
         return DP[0][0];
     }
+//Bottom Up DP
+    public int longestPalindromeSubseq(String s) {
+        int[][] dp = new int[s.length()][s.length()];
+        for (int i = s.length() - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for (int j = i + 1; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[0][s.length() - 1];
+    }
 }
+/*
+Output : Bottom Up
+i=3/2 | j=4/3
+  a b a d
+a 1 1 3 3 
+b   1 1 1
+a     1 1
+d       1
+*/
+
 
 /*
-//Aditya
+//Aditya  
+Pepcoding GAP Strategy
 class Solution {
     public int longestPalindromeSubseq(String s) {
     int [][] dp = new int[s.length()][s.length()];
@@ -72,9 +105,4 @@ class Solution {
     return dp[0][s.length()-1];
     }
 }
-  a b c
-a 1 
-b   1
-c     1
-
 */
