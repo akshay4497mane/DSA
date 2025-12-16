e07fe17e7b6ec6985f8f274c275d266e585c5363
Akshay Mane <akshay4497mane@gmail.com>
2025-12-16 23:29:02 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (26.07%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,3 +1,44 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        //nums = [4,5,6,7,0,1,2], target = 0
+        int l = 0, r=nums.length-1, mid = l+(r-l)/2;
+        while(l<=r){
+            mid = l+(r-l)/2; //0+6/2=3
+            if(nums[mid]==target) return mid;
+            if(nums[l] <= nums[mid]){ // left half is sorted
+                if( target>=nums[l] && target<=nums[mid] ){
+                    r = mid-1;
+                }else{
+                    l = mid+1;
+                }
+            }else{ //right half is sorted
+                if( target>=nums[mid] && target<=nums[r] ){
+                    l = mid+1;
+                }else{
+                    r = mid-1;
+                }                
+            }
+        }
+        return -1;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 Problem Title: Search in Rotated Sorted Array
 Approach(very short): Use binary search to identify the sorted half and adjust search space accordingly.
@@ -10,13 +51,12 @@ https://www.youtube.com/watch?v=w2G2W8l__pc
 
 */
 
-class Solution {
+class Solution1 {
     public int search(int[] nums, int target) {
         int low = 0, high = nums.length - 1, mid = 0;
         
         while (low <= high) {
             mid = low + (high - low) / 2; // Calculate mid to avoid overflow
-            
             if (nums[mid] == target) // If mid element is the target, return index
                 return mid;
 
