aa6225737afe2539bac368c7124a9d7f99cb66d4
Akshay Mane <akshay4497mane@gmail.com>
2025-12-17 00:14:33 +0530

    Time: 4 ms (39.64%) | Memory: 43.6 MB (11.11%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution2 {
     public boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
         for(char ch : s.toCharArray()){
@@ -7,13 +7,33 @@ class Solution {
             else
                 if(!st.isEmpty() &&
                     ( st.peek()=='(' && ch == ')' ||
-                   st.peek()=='{' && ch == '}' ||
-                   st.peek()=='[' && ch == ']'
-                    ))
+                      st.peek()=='{' && ch == '}' ||
+                       st.peek()=='[' && ch == ']')
+                  )
                     st.pop();
                 else
                     return false;
         }
         return st.isEmpty();
+    }
+}
+
+class Solution {
+    public boolean isValid(String s) {
+        HashMap<Character,Character> maps=new HashMap<Character,Character>();
+        maps.put(')','(');
+        maps.put(']','[');
+        maps.put('}','{');
+        Stack<Character> stack=new Stack<Character>();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(maps.containsKey(c)){
+                if(stack.empty()||stack.pop()!=maps.get(c))
+                    return false;
+            }
+            else
+                stack.push(c);
+        }
+        return stack.empty();
     }
 }
