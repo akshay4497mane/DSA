c1e86abcb6864b9ae0a855e566974822ed48a1e4
Akshay Mane <akshay4497mane@gmail.com>
2026-01-02 20:04:27 +0530

    Time: 65 ms (82.42%) | Memory: 48 MB (64.11%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -50,20 +50,15 @@ class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         Set<String> dict = new HashSet<>(wordList);
         if (!dict.contains(endWord)) return 0;
-
         Queue<String> q = new ArrayDeque<>();
         q.offer(beginWord);
         dict.remove(beginWord);
-
         int steps = 1;
-
         while (!q.isEmpty()) {
             int levelSize = q.size();
-
             for (int k = 0; k < levelSize; k++) {
                 String word = q.poll();
                 if (word.equals(endWord)) return steps;
-
                 char[] arr = word.toCharArray();
                 for (int i = 0; i < arr.length; i++) {
                     char original = arr[i];
@@ -71,9 +66,8 @@ class Solution {
                         if (c == original) continue;
                         arr[i] = c;
                         String next = new String(arr);
-
                         if (dict.contains(next)) {
-                            dict.remove(next);
+                            dict.remove(next); //IF we DONT remove, TLE
                             q.offer(next);
                         }
                     }
