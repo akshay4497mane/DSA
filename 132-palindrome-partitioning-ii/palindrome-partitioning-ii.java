e5a11538c9717c5b86da4420c6d4001f521a7335
Akshay Mane <akshay4497mane@gmail.com>
2026-01-02 17:07:35 +0530

    Time: 60 ms (44.87%) | Memory: 82.3 MB (12.38%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
@@ -1,6 +1,6 @@
 class Solution {
     /*
-    Front Partitioning -> 
+    Approach : Front Partitioning, for every index i =0..N-1 -> partition at i | recursive call further
     f(i) -> 
       base case : i==n : return 0
       for j=i-n-1
@@ -13,11 +13,12 @@ class Solution {
     public int minCut(String s) {//Approach 3 : Tabulation       
         int N = s.length();
         DP = new int[N+1]; 
+        memoPalindrome = new Boolean[s.length()][s.length()];
         DP[N] = 0;
         for(int i=N-1; i>=0; i--){
             int minCost = Integer.MAX_VALUE;
             for(int j=i; j<N; j++){
-                if(isPalindrome(i,j,s)){
+                if( isPalindrome(i,j,s) ){
                     int cost = 1 + DP[j+1];
                     minCost = Math.min(minCost, cost);                    
                 }
@@ -28,19 +29,18 @@ class Solution {
     }
 
     String s; int N=0;
-    public int minCut_Memoization(String str) {       
+    public int minCut_Memoization(String str) {    // Approach 1 : Recursive | Approach 2 : Memoization
         s = str; N = s.length();
         DP = new int[N+1]; Arrays.fill(DP, -1);
+        memoPalindrome = new Boolean[s.length()][s.length()];
         return helper(0)-1;
     }
     int[] DP;
     int helper(int index){
         if(DP[index]!=-1) return DP[index];
-        //1.Base case
-        if(index==N) return DP[N] = 0;
+        if(index==N) return DP[N] = 0; // 1.Base case
         int minCost = Integer.MAX_VALUE;
-        //2.Try all possible partitions
-        for(int j=index; j<N; j++){
+        for(int j=index; j<N; j++){ // 2.Try all possible partitions
             if( isPalindrome(index, j, s) ){ //palindrome from i..j | call further F(j+1)
                 int cost = 1 + helper(j+1);
                 minCost = Math.min(minCost, cost);
@@ -48,7 +48,14 @@ class Solution {
         }
         return DP[index] = minCost;
     }
-    boolean isPalindrome(int i, int j, String str){
+    private Boolean memoPalindrome[][];
+    private boolean isPalindrome( int start, int end, String s ) {
+        if (start >= end) return true;
+        if (memoPalindrome[start][end] != null) return memoPalindrome[start][end];
+        return ( memoPalindrome[start][end] = (s.charAt(start) == s.charAt(end)) && isPalindrome(start + 1, end - 1, s) );
+    }
+
+    boolean isPalindrome_OLD(int i, int j, String str){
         while(i<j){
             if(str.charAt(i) != str.charAt(j))
                 return false;
