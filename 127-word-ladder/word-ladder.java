b4f8b48a6b916d67e7894c82b275294b3d2c37cb
Akshay Mane <akshay4497mane@gmail.com>
2026-01-02 19:59:13 +0530

    Time: 68 ms (74.72%) | Memory: 47.9 MB (73.15%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -9,8 +9,8 @@ class Pair {
         this.steps = steps;
     }
 }
-
-class Solution {
+//Using PAIR<String, int>
+class Solution_1 {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         Set<String> dict = new HashSet<>(wordList);
         if (!dict.contains(endWord)) return 0;
@@ -41,6 +41,46 @@ class Solution {
                 }
                 arr[i] = original;
             }
+        }
+        return 0;
+    }
+}
+
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> dict = new HashSet<>(wordList);
+        if (!dict.contains(endWord)) return 0;
+
+        Queue<String> q = new ArrayDeque<>();
+        q.offer(beginWord);
+        dict.remove(beginWord);
+
+        int steps = 1;
+
+        while (!q.isEmpty()) {
+            int levelSize = q.size();
+
+            for (int k = 0; k < levelSize; k++) {
+                String word = q.poll();
+                if (word.equals(endWord)) return steps;
+
+                char[] arr = word.toCharArray();
+                for (int i = 0; i < arr.length; i++) {
+                    char original = arr[i];
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        if (c == original) continue;
+                        arr[i] = c;
+                        String next = new String(arr);
+
+                        if (dict.contains(next)) {
+                            dict.remove(next);
+                            q.offer(next);
+                        }
+                    }
+                    arr[i] = original;
+                }
+            }
+            steps++;
         }
         return 0;
     }
