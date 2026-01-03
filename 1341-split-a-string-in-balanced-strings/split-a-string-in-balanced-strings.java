e306f93c8e47b4dda2d06971e9ca2bf68f65bbd2
Akshay Mane <akshay4497mane@gmail.com>
2026-01-03 14:34:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (89.40%) - LeetSync

--- 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.java
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int balancedStringSplit(String s) {
+    public int balancedStringSplit_1(String s) {
         int i=0, countL=0, countR=0, n = s.length(), ans=0;
         while(i < n){
             if('L' == s.charAt(i)){
@@ -14,6 +14,15 @@ class Solution {
                 countR=0;
             }
             i++;
+        }
+        return ans;
+    }
+
+    public int balancedStringSplit(String s){
+        int bal=0,ans=0;
+        for(int i=0;i<s.length();i++){
+            bal += s.charAt(i)=='R'?1:-1;
+            if(bal==0) ans++;
         }
         return ans;
     }
