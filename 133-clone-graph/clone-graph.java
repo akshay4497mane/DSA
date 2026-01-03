a0c95cd66346fe2d575ad7fff159840e765de2bb
Akshay Mane <akshay4497mane@gmail.com>
2026-01-03 13:59:13 +0530

    Time: 25 ms (72.26%) | Memory: 44.3 MB (57.59%) - LeetSync

--- 133-clone-graph/clone-graph.java
+++ 133-clone-graph/clone-graph.java
@@ -1,55 +1,24 @@
+//Use HashMap to store {oldNode -> newCopyNode }
+
 class Solution {
     Map<Node, Node> map = new HashMap<>(); //oldNode -> newNode
-    public Node cloneGraph(Node node) {
+    // DFS
+    public Node cloneGraph_DFS(Node node) {
         return dfs(node);
     }
     Node dfs(Node node){        
         if(node==null) return null;
         if(map.containsKey(node)) 
             return map.get(node);
-        Node copyNode = new Node(node.val);
-        map.put(node, copyNode);
+        Node newCopyNode = new Node(node.val);
+        map.put(node, newCopyNode);
         for( Node neigh : node.neighbors ){
-            copyNode.neighbors.add( cloneGraph(neigh) );
+            newCopyNode.neighbors.add( cloneGraph(neigh) );
         }
-        return copyNode;
+        return newCopyNode;
     }
-}
-/*
-1 - 2
-|   |
-3 - 4
-*/
 
-
-
-
-
-
-
-
-
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public List<Node> neighbors;
-    public Node() {
-        val = 0;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val) {
-        val = _val;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val, ArrayList<Node> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-}
-*/
-//Use HashMap to store copied 
-class Solution_1 {
+    // BFS
     public Node cloneGraph(Node node) {
         if(node==null) return node;
         Map<Node, Node> map = new HashMap<>();//oldNode -> New Node
@@ -70,21 +39,24 @@ class Solution_1 {
         }
         return map.get(node);
     }
-
-
-    Map<Node, Node> map = new HashMap<>();
-    public Node cloneGraph_DFS(Node node) {
-        return dfs(node);
-    }
-    Node dfs(Node node){
-        if(node==null)return node;
-        if(map.containsKey(node)) 
-            return map.get(node);
-        Node copy = new Node(node.val);
-        map.put(node, copy);
-        for(Node neigh : node.neighbors){
-            copy.neighbors.add(dfs(neigh));
-        }
-        return copy;
-    }   
 }
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
