6a4440dce38ea29e6ff6ea08f6ecfdc943d37e4a
Akshay Mane <akshay4497mane@gmail.com>
2026-01-10 20:30:13 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (36.72%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,34 +1,29 @@
 class Solution {
-    /*
-        n=1  ans: 1
-        n=2 ans: 2(11,2)
-        n=3 ans: 3 ways(111, 12, 21)
-        n=4 ans: 5 ways(111 1, 12 1, 21 1, 11 2, 2 2)
-        ways(n) = ways(n-1) + ways(n-2)
-        Similar to Fibonacci
-     */
-    /*
-    //Recursion  Time: O( 2^N ) | N=45 : Time Limit Exceeded
-    public int climbStairs(int n) {
-        if(n==1) return 1;
-        if(n==2) return 2;
-        return climbStairs(n-1) + climbStairs(n-2);
+    //Approach 1 : Raw Recursion --> accumulate ways
+    int noOfWays = 0, N;
+    public int climbStairs_1(int n) {
+        N=n;
+        dfs( 1 );
+        return noOfWays;
     }
-    */
+    //cannot be memoized as return type is void
+    void dfs( int i ){
+        if(i==N){ noOfWays++; return; }
+        dfs(i+1); 
+        dfs(i+2);
+    }
 
-    //Top Down DP | 
+    //Approach 2 :
     public int climbStairs(int n) {
-        if(n==1) return 1;
-        if(n==2) return 2;
-        int[] waysDP = new int[n+1];
-        for(int i=0;i<=n;i++)
-            waysDP[i] = -1;
-        return ways(n, waysDP);
+        N=n;
+        memo = new int[N];
+        return dfsMemo(0);
     }
-    public int ways(int n, int[] waysDP){
-        if(n==1 || n==2) return n;
-        if(waysDP[n] !=-1) return waysDP[n];
-        waysDP[n] = ways(n-1, waysDP) + ways(n-2, waysDP);
-        return waysDP[n];
+    int[] memo;
+    int dfsMemo(int i){
+        if(i==N) return 1;
+        if(i>N) return 0;
+        if(memo[i] != 0) return memo[i];
+        return memo[i] = dfsMemo(i+1) + dfsMemo(i+2);
     }
 }
