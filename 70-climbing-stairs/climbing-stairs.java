6dd01db8812f3936b8e4e4ea315d72920d9e9cba
Akshay Mane <akshay4497mane@gmail.com>
2026-01-10 20:44:53 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (56.61%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -16,14 +16,30 @@ class Solution {
     //Approach 2 :
     public int climbStairs(int n) {
         N=n;
-        memo = new int[N];
+        memo = new int[N+2];
         return dfsMemo(0);
     }
     int[] memo;
     int dfsMemo(int i){
-        if(i==N) return 1;
-        if(i>N) return 0;
         if(memo[i] != 0) return memo[i];
+        if(i>N) return memo[i]=0;
+        if(i==N) return memo[i]=1;
         return memo[i] = dfsMemo(i+1) + dfsMemo(i+2);
     }
+/*
+//dfsMemo(i) = number of ways to reach step N starting from step i
+//dp[i] = number of ways to reach N from i
+//Approach 3
+[0 1 2]
+n = 3
+int[] DP;  [0, 0, 0, 0] | N=5 | dp[3] = 3->5 
+//DP[i] = how many ways you can go from i -> N?
+DP[3] = 1
+DP[2] = 1
+DP[1] = 2
+DP[0] = 3
+
+climbStairs(3) -> 3
+climb(1) -> 1
+*/
 }
