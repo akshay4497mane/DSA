0840ad8d245333efb5ebb3ea826997b077e2081f
Akshay Mane <akshay4497mane@gmail.com>
2026-01-16 14:42:42 +0530

    Time: 146 ms (47.44%) | Memory: 43.1 MB (49.38%) - LeetSync

---
+++ 79-word-search/word-search.java
@@ -0,0 +1,53 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int rows = board.length;
+        int cols = board[0].length;
+
+        boolean[][] visited = new boolean[rows][cols];
+
+        // Try every cell as a starting point
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (dfs(board, word, i, j, 0, visited))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(
+            char[][] board,
+            String word,
+            int r,
+            int c,
+            int index,
+            boolean[][] visited) {
+
+        // All characters matched
+        if (index == word.length())
+            return true;
+
+        // Boundary check
+        if (r < 0 || c < 0 || r >= board.length || c >= board[0].length)
+            return false;
+
+        // Already used or mismatch
+        if (visited[r][c] || board[r][c] != word.charAt(index))
+            return false;
+
+        // Mark visited
+        visited[r][c] = true;
+
+        // Explore all 4 directions
+        boolean found =
+                dfs(board, word, r + 1, c, index + 1, visited) ||
+                dfs(board, word, r - 1, c, index + 1, visited) ||
+                dfs(board, word, r, c + 1, index + 1, visited) ||
+                dfs(board, word, r, c - 1, index + 1, visited);
+
+        // Backtrack
+        visited[r][c] = false;
+
+        return found;
+    }
+}
