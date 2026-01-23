cf5f25bc2e67ac87df91d070890f58cd9376c1a8
Akshay Mane <akshay4497mane@gmail.com>
2026-01-23 23:39:42 +0530

    Time: 34 ms (63.05%) | Memory: 74.7 MB (30.94%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
@@ -18,22 +18,43 @@ class Solution {
         return dfs(0);                
     }
     //Binary Search
-    private static int dfs_binary(int i){
+    private static int dfs(int i){
         //base
         if( i==start.length) return 0;
         if( DP[i] != null ) return DP[i];
         //skip job
         int skipAns = dfs(i+1);
 
-        //pick
-        int nextJob = Arrays.binarySearch(start, end[i]);
-        if(nextJob<0) nextJob = -nextJob -1;        
-
+        //pick/take the job
+        int nextJob = nextIndex(i);
+        //int nextJob = Arrays.binarySearch(start, end[i]); //WRONG
+        //int nextJob = Arrays.binarySearch(start, i + 1, start.length, end[i]);//Failed for [1,2,2,3]
+        //if (nextJob < 0) nextJob = -nextJob - 1;
+        // nextJob is guaranteed >= i+1
         int ansTake = dfs(nextJob) + profit[i];
         return DP[i] = Math.max( skipAns, ansTake );
     }
+    private static int nextIndex(int i) {
+        int lo = i + 1, hi = start.length;
+        int target = end[i];
+
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (start[mid] < target) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+        return lo;
+    }
+
     //Linear Search
-    private static int dfs(int i){
+    /* 
+Runtime 906 ms Beats 9.34%
+Memory 74.64 MB Beats 30.94%
+    */
+    private static int dfs_linear(int i){
         //base
         if( i==start.length) return 0;
         if( DP[i] != null ) return DP[i];
