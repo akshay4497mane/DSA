46e58f50d86275871c7a6b21f2ada309760650b2
Akshay Mane <akshay4497mane@gmail.com>
2026-02-16 14:43:29 +0530

    Update trapping-rain-water.java

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,73 +1,9 @@
 class Solution {
     //min(Lmax, Rmax) - currHeight
-    public int trap(int[] height) {
-        int N = height.length;
-        int[] LMAX = new int[N], RMAX = new int[N];
-        int currMax = height[0];
-        for(int i=0;i<N; i++){
-            currMax = Math.max(currMax, height[i]);
-            LMAX[i] = currMax;
-        }
-        currMax = height[N-1];
-        for(int i=N-1;i>=0; i--){
-            currMax = Math.max(currMax, height[i]);
-            RMAX[i] = currMax;
-        }
-        int sumAns = 0;
-        for(int i=0; i<N;i++){
-            sumAns += Math.min(LMAX[i], RMAX[i]) - height[i];
-        }
-        return sumAns;
-        /*
-        for(int i=0; i<N;i++){
-            System.out.print(LMAX[i] + " ");
-        }
-        System.out.println(" ");
-        for(int i=0; i<N;i++){
-            System.out.print(RMAX[i] + " ");
-        }*/
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //Revision 8 Nov 2025 //Coding 12 Nov
+//Revision 16 feb 2026
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* Approach 3 : Two Pointer Approach | Time : O(N) | Space: O(1)
-*/
+/* Approach 4 : Two Pointer Approach | Time : O(N) | Space: O(1)   */
     public int trap_approach3(int[] height) {
         int N = height.length, ans = 0, left=0, right=N-1, leftMax = 0, rightMax = 0;
         while( left < right ){
@@ -90,6 +26,27 @@ class Solution {
         return ans;
     }
 
+    //Approach 3 : Dont use LMAX, use only RMAX => calculate on the fly
+    public int trap(int[] height) {
+        int n = height.length;
+        if (n == 0) return 0;
+        int ans = 0;
+        // Step 1: Precompute RMAX
+        int[] rMax = new int[n];
+        int maxTillNow = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            maxTillNow = Math.max(maxTillNow, height[i]);
+            rMax[i] = maxTillNow;
+        }
+        // Step 2: Traverse from left
+        int leftMax = 0;
+        for (int i = 0; i < n; i++) {
+            leftMax = Math.max(leftMax, height[i]); // Update leftMax dynamically
+            ans += Math.min(leftMax, rMax[i]) - height[i];         // Water trapped at index i
+        }
+        return ans;
+    }
+    
     /*Approach 2 : DP approach | Precalculate & Store LMAX, RMAX 
     for every i(1 to N-2), find LMAX, RMAX,  
     find sum of all answers ( min(lmax, rmax) - h[i] )
