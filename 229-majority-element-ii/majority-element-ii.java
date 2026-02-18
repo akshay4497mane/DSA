0eb1484499f5065df9ecda21d8aede6b6d294e2f
Akshay Mane <akshay4497mane@gmail.com>
2026-02-18 19:02:53 +0530

    Time: 2 ms (99.67%) | Memory: 52.8 MB (38.52%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,58 +1,45 @@
 class Solution {
-    public List<Integer> majorityElement(int[] A) {
-        int x = 0, xCount=0, y=0, yCount=0, n = A.length;
-        for(int i=0; i<n; i++){
-            if(xCount==0 && A[i]!=y){ //ERROR IN FINDING THIS EDGE CASE
-                x= A[i];
-                xCount++;
-            }else if(A[i]==x){
-                xCount++;
-            }else if(yCount==0 && A[i]!=x){
-                y=A[i];
-                yCount++;
-            }else if(A[i]==y){
-                yCount++;
-            }else{ 
-                xCount--;
-                yCount--;
+/*
+Approach 1: brute force, map, 
+Approach 2 : Sort -> traverse count 
+=> N log N +  N | O(1)
+
+Aproach 3: 
+    cand1 , count1
+    cand2, count2
+ if( ==cand1) count1++;
+ else if( ==cand2) count2++;
+ check count1 =0
+ check count2 ==0
+ --count1, --count2
+
+reverify cand1, cand2
+*/    
+    public List<Integer> majorityElement(int[] nums) {
+        int cand1=0 , count1=0, cand2=0, count2=0;
+        for(int x : nums){
+            if( x == cand1 ) count1++;
+            else if( x == cand2 ) count2++;
+            else if( count1 == 0 ){
+                cand1 = x;
+                count1 = 1;
+            }else if( count2 == 0 ){
+                cand2 = x;
+                count2 = 1;
+            }else{
+                --count1; --count2;
             }
         }
-        xCount =0; yCount=0;
-        for(int i=0;i<n; i++){
-            if(A[i]==x) //ERROR IN FINDING THIS EDGE CASE
-                xCount += (A[i]==x) ? 1 :0;
-            else
-                yCount += (A[i]==y) ? 1 :0;
+        //reverify cand1, cand2               
+        count1=0; count2=0;
+        for(int x : nums){
+            if( x == cand1 ) count1++;
+            else if( x == cand2 ) count2++;
         }
         List<Integer> ans = new ArrayList<>();
-        if(3 * xCount > n) ans.add(x);
-        if(3 * yCount > n) ans.add(y);
+        int limit = nums.length / 3;
+        if( count1> limit) ans.add(cand1);
+        if( count2> limit) ans.add(cand2);
         return ans;
     }
-
-/*[0,0,0]. => failed added
-Wrong Answer. | 86 / 92 testcases passed
-Input nums = [0,0,0]
-Use Testcase
-Output [0,0]
-Expected [0]
-Added if/else in for loop
-*/
-/*
-Wrong Answer
-90 / 92 testcases passed
-
-Input nums = [2,1,1,3,1,4,5,6]
-
-Use Testcase
-Output
-[]
-Expected
-[1]
-// [2,1,1,3,>1,4,5,6]
-x 2-0  | 1-1
-y 1-2
-Solution : && A[i]!=y needs to be added when xCount==0
-
-*/
 }
